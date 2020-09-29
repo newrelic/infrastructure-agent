3d9560cede1e074b5bad0e1ba8d5dc1530c903ad
varas <jhvaras@gmail.com>
2020-09-29 11:12:49 +0200

    docs: docblock typo

--- pkg/integrations/v4/manager.go
+++ pkg/integrations/v4/manager.go
@@ -31,7 +31,7 @@ import (
 var illog = log.WithComponent("integrations.Manager")
 
 const (
-	// In VersionAwareEmitter Integrations config, we use the "instances" key word
+	// Integrations V3 configs use the "instances" key word
 	// In the current Integrations config, we use "integrations"
 	// The two fields below will allows us distinguish between both, to ignore V4
 	integrationsField    = "integrations"
