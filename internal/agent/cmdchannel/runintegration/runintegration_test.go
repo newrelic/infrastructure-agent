940749686e5aba195f832df53d16f975e4cfe7b3
varas <jhvaras@gmail.com>
2020-10-21 19:06:02 +0200

    feat: run integration cmd handler, TODO inject

--- internal/agent/cmdchannel/runintegration/runintegration_test.go
+++ internal/agent/cmdchannel/runintegration/runintegration_test.go
@@ -7,12 +7,19 @@ import (
 	"testing"
 
 	"github.com/newrelic/infrastructure-agent/internal/agent/cmdchannel"
+	"github.com/newrelic/infrastructure-agent/internal/integrations/v4/integration"
 	"github.com/newrelic/infrastructure-agent/pkg/backend/commandapi"
+	"github.com/newrelic/infrastructure-agent/pkg/log"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+var (
+	l = log.WithComponent("test")
 )
 
 func TestHandle_returnsErrorOnMissingIntegrationName(t *testing.T) {
-	h := NewHandler()
+	h := NewHandler(make(chan integration.Definition, 1), integration.ErrLookup, l)
 
 	cmdArgsMissingName := commandapi.Command{
 		Args: []byte(`{ "integration_args": ["foo", "bar"] }`),
@@ -20,4 +27,28 @@ func TestHandle_returnsErrorOnMissingIntegrationName(t *testing.T) {
 
 	_, err := h.Handle(context.Background(), cmdArgsMissingName, false)
 	assert.Equal(t, cmdchannel.NewArgsErr(NoIntNameErr).Error(), err.Error())
+}
+
+func TestHandle_queuesIntegrationToBeRun(t *testing.T) {
+	defQueue := make(chan integration.Definition, 1)
+	il := integration.InstancesLookup{
+		Legacy: func(_ integration.DefinitionCommandConfig) (integration.Definition, error) {
+			return integration.Definition{}, nil
+		},
+		ByName: func(_ string) (string, error) {
+			return "/path/to/nri-foo", nil
+		},
+	}
+	h := NewHandler(defQueue, il, l)
+
+	cmd := commandapi.Command{
+		Args: []byte(`{ "integration_name": "nri-foo", "integration_args": ["bar", "baz"] }`),
+	}
+
+	_, err := h.Handle(context.Background(), cmd, false)
+	require.NoError(t, err)
+
+	d := <-defQueue
+	assert.Equal(t, "nri-foo", d.Name)
+	// Definition won't allow assert further
 }
