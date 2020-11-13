226d99b14231c9b38277ebbb5cbfe297361d3c7b
jhvaras <jhvaras@gmail.com>
2020-11-13 10:05:16 +0100

    chore: remove dead code (#218)

--- pkg/metrics/storage/storage_sampler.go
+++ pkg/metrics/storage/storage_sampler.go
@@ -333,13 +333,15 @@ func (c *PartitionsCache) Get() ([]PartitionStat, error) {
 	if c.lastStat != nil && now.Before(c.lastInvocation.Add(c.ttl)) {
 		return c.lastStat, nil
 	}
+
 	var err error
 	c.lastStat, err = c.refresh()
 	if err != nil {
 		c.lastStat = nil
 	}
 	c.lastInvocation = now
-	return c.lastStat, err
+
+	return c.lastStat, nil
 }
 
 func (c *PartitionsCache) refresh() ([]PartitionStat, error) {
