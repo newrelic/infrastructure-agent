1a8c437ae4cd6f6745d2bce52874940e2f1dcde4
varas <jhvaras@gmail.com>
2020-11-23 15:20:56 +0100

    fix: build flag

--- internal/os/distro/distro_unix.go
+++ internal/os/distro/distro_unix.go
@@ -1,6 +1,7 @@
 // Copyright 2020 New Relic Corporation. All rights reserved.
 // SPDX-License-Identifier: Apache-2.0
 // +build linux darwin
+
 package distro
 
 import (
--- internal/os/distro/distro_windows.go
+++ internal/os/distro/distro_windows.go
@@ -1,6 +1,7 @@
 // Copyright 2020 New Relic Corporation. All rights reserved.
 // SPDX-License-Identifier: Apache-2.0
 // +build windows
+
 package distro
 
 func GetDistro() string {
