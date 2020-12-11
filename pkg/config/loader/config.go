94fb7a9c9b1623006c3f7898fe2eca9dfd33b959
varas <jhvaras@gmail.com>
2020-12-11 12:30:16 +0100

    feat: ExpandEnvVars

--- pkg/config/loader/config.go
+++ pkg/config/loader/config.go
@@ -4,8 +4,12 @@
 package config_loader
 
 import (
+	"fmt"
 	"io/ioutil"
 	"os"
+	"regexp"
+	"strconv"
+	"strings"
 
 	"gopkg.in/yaml.v2"
 )
@@ -34,6 +38,11 @@ func LoadYamlConfig(configObject interface{}, configFilePaths ...string) (*YAMLM
 			defer fd.Close()
 
 			rawConfig, err := ioutil.ReadAll(fd)
+			if err != nil {
+				return nil, err
+			}
+
+			rawConfig, err = ExpandEnvVars(rawConfig)
 			if err != nil {
 				return nil, err
 			}
@@ -75,4 +84,42 @@ func fileExists(filename string) bool {
 		}
 	}
 	return true
+}
+
+func ExpandEnvVars(content []byte) ([]byte, error) {
+	r := regexp.MustCompile(`({{ *\w+.*?}})`)
+	matches := r.FindAllIndex(content, -1)
+
+	if len(matches) == 0 {
+		return content, nil
+	}
+
+	var newContent []byte
+	var lastReplacement int
+	for _, idx := range matches {
+		evStart := idx[0] + 2 // drop {{
+		evEnd := idx[1] - 2   // drop }}
+		if len(content) < evStart || len(content) < evEnd {
+			return content, fmt.Errorf("cannot replace configuration environment variables")
+		}
+
+		evName := strings.TrimSpace(string(content[evStart:evEnd]))
+		if evVal, exist := os.LookupEnv(evName); exist {
+			// quote non numerics
+			if _, err := strconv.ParseFloat(evVal, 64); err != nil {
+				evVal = fmt.Sprintf(`"%s"`, evVal)
+			}
+			newContent = append(newContent, content[lastReplacement:idx[0]]...)
+			newContent = append(newContent, []byte(evVal)...)
+			lastReplacement = idx[1]
+		} else {
+			return nil, fmt.Errorf("cannot replace configuration environment variables, missing env-var: %s", evName)
+		}
+	}
+
+	if lastReplacement != len(content) {
+		newContent = append(newContent, content[lastReplacement:]...)
+	}
+
+	return newContent, nil
 }
