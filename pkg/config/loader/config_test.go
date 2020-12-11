1179761a1754fec699b7245fe87ea5e6dce618c2
jhvaras <jhvaras@gmail.com>
2020-12-11 17:51:57 +0100

    Environment variables replacement for agent config (#263)
    * test: LoadYamlConfig with databind variables
    
    * test: fix test for configureLogRedirection
    
    * refactor: extract databind "variables" YAML section
    
    * feat: scaffold databind "variables" addition to agent config, no feature added for now
    
    * feat: replacement WIP
    
    * test: add metadata assertion to TestLoadYamlConfig_withDatabindVariables
    
    * test: use YAMAgentConfig
    
    * fix: wrong debug field on legacy runner
    
    * fix: databind panics
    
    * fix: databind panic on nil pointers
    
    * feat: agent config support for dtabind variables
    
    * fix: remove AgentMode type
    
    * test: skip TestEventsPluginRunV1 on Windows because of random timeouts on CI
    
    * feat: paremetrize databind test provider
    
    * feat: databind test provider non typed
    
    * feat: ExpandEnvVars

--- pkg/config/loader/config_test.go
+++ pkg/config/loader/config_test.go
@@ -91,3 +91,38 @@ baz: ${creds.user}
 	require.Contains(t, cfg.Databind.Variables, "creds")
 	assert.Equal(t, cfg.Databind.Variables["creds"].Vault.HTTP.URL, "http://my.vault.host/v1/newengine/data/secret")
 }
+
+func TestExpandEnvVars(t *testing.T) {
+	emptyEnv := map[string]string{}
+
+	tests := []struct {
+		name    string
+		env     map[string]string
+		content string
+		want    string
+		wantErr bool
+	}{
+		{"empty", emptyEnv, "", "", false},
+		{"no placeholder", emptyEnv, "foo bar\nbaz", "foo bar\nbaz", false},
+		{"1 placeholder with no env-var", emptyEnv, "foo: \"{{BAR}}\"\nbaz", "", true},
+		{"1 placeholder with 1 env-var", map[string]string{"BAR": "VAL"}, "foo: {{BAR}}\nbaz", "foo: \"VAL\"\nbaz", false},
+		{"1 placeholder with 1 env-var with spaces", map[string]string{"BAR": "VAL"}, "foo: {{  BAR  }}\nbaz", "foo: \"VAL\"\nbaz", false},
+		{"3 placeholder with 1 env-var", map[string]string{"BAR": "VAL"}, "foo: {{BAR}}\nbaz: {{BAR}}-{{BAR}}", "foo: \"VAL\"\nbaz: \"VAL\"-\"VAL\"", false},
+		{"2 placeholder with 2 env-var", map[string]string{"BAR1": "VAL1", "BAR2": "VAL2"}, "foo: {{BAR1}}\nbaz: {{BAR2}}", "foo: \"VAL1\"\nbaz: \"VAL2\"", false},
+		{"1 placeholder with 1 env-var special chars", map[string]string{"BAR": "$.*^"}, "foo: {{BAR}}\nbaz", "foo: \"$.*^\"\nbaz", false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			for k, v := range tt.env {
+				require.NoError(t, os.Setenv(k, v))
+			}
+			gotContent, gotErr := ExpandEnvVars([]byte(tt.content))
+			if tt.wantErr {
+				require.Error(t, gotErr)
+			} else {
+				require.NoError(t, gotErr)
+			}
+			assert.Equal(t, tt.want, string(gotContent))
+		})
+	}
+}
