69d762f262d45a93b12f0dbf449322aaaf53c22b
jhvaras <jhvaras@gmail.com>
2020-12-15 15:03:47 +0100

    Update pkg/integrations/track/ctx/ctx.go
    Co-authored-by: Carlos <croman@newrelic.com>

--- pkg/integrations/track/ctx/ctx.go
+++ pkg/integrations/track/ctx/ctx.go
@@ -1,3 +1,6 @@
+// Copyright 2020 New Relic Corporation. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
 package ctx
 
 // CmdChannelRequest DTO storing context required to handle actions on integration run exit.
