7a7d02c0c58d2137b68fc75aed1665cdd3b85cc4
jhvaras <jhvaras@gmail.com>
2021-01-28 11:48:48 +0100

    fix: cmd API allow list (#299)

--- pkg/integrations/cmdapi/cmdapi_test.go
+++ pkg/integrations/cmdapi/cmdapi_test.go
@@ -12,15 +12,15 @@ func TestIsAllowedToRunStopFromCmdAPI(t *testing.T) {
 		integrationName string
 		want            bool
 	}{
-		{"empty", "", false},
-		{"allowed 1", "nri-process-detector", true},
-		{"allowed 2", "nri-lsi-java", true},
-		{"not allowed", "nri-foo", false},
-		{"extra suffix", "nri-lsi-java-foo", false},
+		{"empty", "", true},
+		{"allowed 1", "nri-process-discovery", false},
+		{"allowed 2", "nri-lsi-java", false},
+		{"not allowed", "nri-foo", true},
+		{"extra suffix", "nri-lsi-java-foo", true},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			assert.Equal(t, tt.want, IsAllowedToRunStopFromCmdAPI(tt.integrationName))
+			assert.Equal(t, tt.want, IsForbiddenToRunStopFromCmdAPI(tt.integrationName))
 		})
 	}
 }
