b43fe7a54a69a6b9142803ce4581e63c38c0481c
varas <jhvaras@gmail.com>
2021-04-29 12:26:16 +0200

    test: packaging initial install (wip)

---
+++ test/packaging/test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ "$ANSIBLE_INVENTORY" = "" ]; then
+  printf "Error: missing required env-var: %s\n" "ANSIBLE_INVENTORY"
+  exit 1
+fi
+
+if [ "$NR_LICENSE_KEY" = "" ]; then
+  printf "Error: missing required env-var: %s\n" "NR_LICENSE_KEY"
+  exit 1
+fi
+
+printf "\nTesting initial install...\n"
+NR_LICENSE_KEY="$NR_LICENSE_KEY" INITIAL_AGENT_VERSION=1.16.1 ansible-playbook -i "$ANSIBLE_INVENTORY" test/packaging/ansible/test.yml
