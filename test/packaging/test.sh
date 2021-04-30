ff9d70ec52b69d29bfc83e196918e5cd96955254
varas <jhvaras@gmail.com>
2021-04-30 12:05:02 +0200

    refactor: move env lookups into vars file

--- test/packaging/test.sh
+++ test/packaging/test.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# 1.18.0 is the minimum version for ARM.
-INITIAL_AGENT_VERSION=1.18.0
-
 if [ "$ANSIBLE_INVENTORY" = "" ]; then
   printf "Error: missing required env-var: %s\n" "ANSIBLE_INVENTORY"
   exit 1
@@ -14,4 +11,4 @@ if [ "$NR_LICENSE_KEY" = "" ]; then
 fi
 
 printf "\nTesting initial install...\n"
-NR_LICENSE_KEY="$NR_LICENSE_KEY" INITIAL_AGENT_VERSION="$INITIAL_AGENT_VERSION" ansible-playbook -i "$ANSIBLE_INVENTORY" test/packaging/ansible/test.yml
+NR_LICENSE_KEY="$NR_LICENSE_KEY" ansible-playbook -i "$ANSIBLE_INVENTORY" test/packaging/ansible/test.yml
