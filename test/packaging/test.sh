2a1eaa1141f98ae39d23b841986d5e81a072a94e
Ruben Ruiz de Gauna <rubenruizdegauna@gmail.com>
2021-05-14 11:25:07 +0200

    test: packaging test integrations in container

--- test/packaging/test.sh
+++ test/packaging/test.sh
@@ -11,4 +11,24 @@ if [ "$NR_LICENSE_KEY" = "" ]; then
 fi
 
 printf "\nTesting initial install...\n"
-NR_LICENSE_KEY="$NR_LICENSE_KEY" ansible-playbook -i "$ANSIBLE_INVENTORY" test/packaging/ansible/test.yml
+if ! NR_LICENSE_KEY="$NR_LICENSE_KEY" ansible-playbook -i "$ANSIBLE_INVENTORY" test/packaging/ansible/test.yml; then
+  printf "\nRunning the test suite failed"
+  exit 1
+fi
+
+printf "\nVerify integrations in docker are run in expected arch...\n"
+
+if ! docker run --rm --entrypoint /var/db/newrelic-infra/newrelic-integrations/bin/nri-prometheus newrelic/infrastructure "--help"; then
+  printf "\nFailed running integration nri-prometheus"
+  exit 1
+fi
+
+if ! docker run --rm --entrypoint /var/db/newrelic-infra/newrelic-integrations/bin/nri-flex newrelic/infrastructure "--help"; then
+  printf "\nFailed running integration nri-flex"
+  exit 1
+fi
+
+if ! docker run --rm --entrypoint /var/db/newrelic-infra/newrelic-integrations/bin/nri-docker newrelic/infrastructure "-show_version"; then
+  printf "\nFailed running integration nri-docker"
+  exit 1
+fi
