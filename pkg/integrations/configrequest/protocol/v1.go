6f13cab33af362a517addc0d88f5fed22f21af2d
Guillermo Sanchez Gavier <gsanchez@newrelic.com>
2021-05-17 12:06:15 +0200

    core: move protocol example to v1

--- pkg/integrations/configrequest/protocol/v1.go
+++ pkg/integrations/configrequest/protocol/v1.go
@@ -8,6 +8,46 @@ import (
 	"github.com/newrelic/infrastructure-agent/pkg/integrations/v4/config"
 )
 
+/* The protocol is used to register integrations without the need to use config yaml files.
+It wraps the objects used in the configuration files to define variables and integrations.
+Config protocol example.
+{
+	"config_protocol_version": "1",
+	"action": "register_config",
+	"config_name": "myconfig",
+	"config": {
+	  "variables": {
+		"creds": {
+		  "vault": {
+			"http": {
+			  "url": "http://my.vault.host/v1/newengine/data/secret",
+			  "headers": {
+				"X-Vault-Token": "my-vault-token"
+			  }
+			}
+		  }
+		}
+	  },
+	  "integrations": [
+		{
+		  "name": "nri-mysql",
+		  "interval": "15s",
+		  "env": {
+			"PORT": "3306",
+			"USERNAME": "${creds.username}",
+			"PASSWORD": "${creds.password}"
+		  }
+		},
+		{
+		  "name": "long-running-integration",
+		  "timeout": "0"
+		  "exec": "python /opt/integrations/my-script.py --host=127.0.0.1"
+		}
+	  ]
+	}
+}
+*/
+
 type v1 struct {
 	Action     string `json:"action"`
 	ConfigName string `json:"config_name"`
