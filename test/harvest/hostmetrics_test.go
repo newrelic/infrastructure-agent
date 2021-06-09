74eea9e97e33c3140fee1aca2164fc6a5a3ca227
cristianciutea <cristi.ciutea@gmail.com>
2021-06-09 11:43:19 +0200

    tests: increase the amount of data written to the file

--- test/harvest/hostmetrics_test.go
+++ test/harvest/hostmetrics_test.go
@@ -113,8 +113,8 @@ func TestHostCachedMemory(t *testing.T) {
 	defer os.Remove(f.Name())
 
 	// Force memory spike
-	for i := 0; i < 1e7; i++ {
-		f.Write([]byte("0"))
+	for i := 0; i < 1e6; i++ {
+		f.Write([]byte("0000000000"))
 	}
 
 	f.Sync()
