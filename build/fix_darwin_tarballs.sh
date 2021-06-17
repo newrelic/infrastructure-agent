430855cd6827e2a3753bf480807e37085cec07e3
cristianciutea <cristi.ciutea@gmail.com>
2021-06-17 16:33:41 +0200

    feat: added tarball for darwin build

--- build/fix_darwin_tarballs.sh
+++ build/fix_darwin_tarballs.sh
@@ -6,14 +6,14 @@ set -e
 #
 #
 
-for tarball_dirty in $(find dist -regex ".*linux.*_dirty\.\(tar.gz\)");do
+for tarball_dirty in $(find dist -regex ".*darwin.*_dirty\.\(tar.gz\)");do
   tarball=${tarball_dirty:5:${#tarball_dirty}-(5+13)} # Strips begining and end chars
   tarball="${tarball}.tar.gz"
   tarballTmpPath="dist/tarball_temp"
   tarballContentPath="${tarballTmpPath}/newrelic-infra"
 
-  mkdir -p ${tarballContentPath}/etc/{newrelic-infra/integrations.d,init_scripts/{systemd,sysv,upstart}}
-  mkdir -p ${tarballContentPath}/usr/bin
+  mkdir -p ${tarballContentPath}/etc/newrelic-infra/integrations.d
+  mkdir -p ${tarballContentPath}/usr/local/bin
   mkdir -p ${tarballContentPath}/var/{db/newrelic-infra,log/newrelic-infra,run/newrelic-infra}
   mkdir -p ${tarballContentPath}/var/db/newrelic-infra/{custom-integrations,integrations.d,newrelic-integrations}
 
@@ -21,15 +21,11 @@ for tarball_dirty in $(find dist -regex ".*linux.*_dirty\.\(tar.gz\)");do
   tar -xvf ${tarball_dirty} -C ${tarballContentPath}
 
   echo "===> Move files inside ${tarball}"
-  mv ${tarballContentPath}/newrelic-infra "${tarballContentPath}/usr/bin/"
-  mv ${tarballContentPath}/newrelic-infra-service "${tarballContentPath}/usr/bin/"
-  mv ${tarballContentPath}/newrelic-infra-ctl "${tarballContentPath}/usr/bin/"
+  mv ${tarballContentPath}/newrelic-infra "${tarballContentPath}/usr/local/bin/"
+  mv ${tarballContentPath}/newrelic-infra-service "${tarballContentPath}/usr/local/bin/"
+  mv ${tarballContentPath}/newrelic-infra-ctl "${tarballContentPath}/usr/local/bin/"
 
   cp build/package/binaries/linux/config_defaults.sh "${tarballContentPath}/"
-  cp build/package/systemd/newrelic-infra.service "${tarballContentPath}/etc/init_scripts/systemd/"
-  cp build/package/sysv/deb/newrelic-infra  "${tarballContentPath}/etc/init_scripts/sysv/"
-  cp build/package/upstart/newrelic-infra  "${tarballContentPath}/etc/init_scripts/upstart/"
-  cp build/package/binaries/linux/installer.sh "${tarballContentPath}/"
   cp assets/examples/infrastructure/LICENSE.linux.txt "${tarballContentPath}/var/db/newrelic-infra/LICENSE.txt"
 
   echo "===> Creating tarball ${TARBALL_CLEAN}"
