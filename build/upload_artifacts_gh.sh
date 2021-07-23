9a79b6bc265ea238fae488ad8db411091b32c1d6
Ruben Ruiz de Gauna <rubenruizdegauna@gmail.com>
2021-07-23 09:22:54 +0200

    fix: run macos pre-release in macos runner

--- build/upload_artifacts_gh.sh
+++ build/upload_artifacts_gh.sh
@@ -6,7 +6,7 @@ set -e
 #
 #
 cd dist
-for filename in $(find  -regex ".*\.\(msi\|rpm\|deb\|zip\|tar.gz\)");do
+for filename in $(find . -name "*.msi" -o -name "*.rpm" -o -name "*.deb" -o -name "*.zip" -o -name "*.tar.gz");do
   echo "===> Uploading to GH $TAG: ${filename}"
       gh release upload $TAG $filename
 done
