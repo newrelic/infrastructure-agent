848ce2a01d9cd2f1ca79fa34e5524d37d3b6526a
cristianciutea <cristi.ciutea@gmail.com>
2021-08-25 11:44:59 +0200

    tools: added canaries provisioning tools

---
+++ tools/spin-ec2/README.md
@@ -0,0 +1,22 @@
+# AWS tools for infrastructure-agent
+spin-ec2 tools is used for provisioning machines in AWS
+
+## Examples
+The following example shows how to deploy infrastructure agent canaries:
+
+```bash
+    go build .
+    # Display help page
+    ./spin-ec2 -i=false canaries -h
+
+    # Export AWS configuration
+    export AWS_PROFILE=<profile>
+    export export AWS_REGION=<region>
+
+    # Provision canaries
+    ./spin-ec2 -i=false canaries provision -v v<agent_version> -l <license_key>
+
+    # Cleanup old machines. This command will terminate all the instances except the ones
+    # that have the latest 2 versions of infra-agent installed.
+    ./spin-ec2 -i=false canaries prune
+```
---
+++ tools/spin-ec2/aws_test.go
@@ -0,0 +1,115 @@
+package main
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestDetectVersions_Valid(t *testing.T) {
+	instances := map[string]string{
+		"i-042ab6db828c1c318": "canary:v1.2.3:amd64:ubuntu20.04",
+		"i-042ab6db828c1c319": "canary:v1.2.3:arm64:ubuntu20.04",
+		"i-042ab6db828c1c314": "canary:v0.1.3:amd64:ubuntu20.04",
+		"i-042ab6db828c1c315": "canary:v0.1.3:arm64:ubuntu20.04",
+		"i-042ab6db828c1c316": "canary:v1.1.3:amd64:ubuntu20.04",
+		"i-042ab6db828c1c317": "canary:v1.1.3:arm64:ubuntu20.04",
+	}
+
+	expected := []string{
+		"v0.1.3",
+		"v1.1.3",
+		"v1.2.3",
+	}
+
+	actual, err := detectVersions(instances)
+	assert.NoError(t, err)
+
+	assert.Equal(t, expected, actual)
+}
+
+func TestDetectVersions_Invalid(t *testing.T) {
+	instances := map[string]string{
+		"i-042ab6db828c1c314": "canary:v0.1.a3:amd64:ubuntu20.04",
+		"i-042ab6db828c1c315": "canary:0.1.3:arm64:ubuntu20.04",
+	}
+
+	actual, err := detectVersions(instances)
+	assert.Error(t, err)
+
+	var expected []string
+	assert.Equal(t, expected, actual)
+}
+
+func TestGetVersionsToRemove(t *testing.T) {
+	actual := getVersionsToRemove([]string{
+		"v1.1.1",
+		"v2.2.2",
+		"v3.3.3",
+	})
+
+	expected := []string{
+		"v1.1.1",
+	}
+
+	assert.Equal(t, expected, actual)
+
+	actual = getVersionsToRemove([]string{
+		"v1.1.1",
+		"v2.2.2",
+	})
+
+	expected = []string{}
+	assert.Equal(t, expected, actual)
+}
+
+func TestGetInstancesToPrune(t *testing.T) {
+	instances := map[string]string{
+		"i-042ab6db828c1c318": "canary:v1.2.3:amd64:ubuntu20.04",
+		"i-042ab6db828c1c319": "canary:v1.2.3:arm64:ubuntu20.04",
+		"i-042ab6db828c1c314": "canary:v0.1.3:amd64:ubuntu20.04",
+		"i-042ab6db828c1c315": "canary:v0.1.3:arm64:ubuntu20.04",
+		"i-042ab6db828c1c316": "canary:v1.1.3:amd64:ubuntu20.04",
+		"i-042ab6db828c1c317": "canary:v1.1.3:arm64:ubuntu20.04",
+	}
+
+	expected := []string{
+		"i-042ab6db828c1c314",
+		"i-042ab6db828c1c315",
+	}
+
+	actual, err := getInstancesToPrune(instances)
+	assert.NoError(t, err)
+
+	assert.ElementsMatch(t, expected, actual)
+}
+
+func TestParseAWSInstancesValid(t *testing.T) {
+	awsResponse := `i-042ab6db828c1c318
+Name	canary:v1.2.3:amd64:ubuntu20.04
+i-055adb7d536497654
+Name	canary:v1.2.4:amd64:ubuntu20.04
+`
+	expected := map[string]string{
+		"i-042ab6db828c1c318": "canary:v1.2.3:amd64:ubuntu20.04",
+		"i-055adb7d536497654": "canary:v1.2.4:amd64:ubuntu20.04",
+	}
+
+	actual, err := parseAWSInstances(awsResponse)
+
+	assert.NoError(t, err)
+	assert.Equal(t, expected, actual)
+}
+
+func TestParseAWSInstancesInvalid(t *testing.T) {
+	awsResponse := `i-042ab6db828c1c318
+canary:v1.2.3:amd64:ubuntu20.04
+i-055adb7d536497654
+Name	canary:v1.2.4:amd64:ubuntu20.04
+`
+	var expected map[string]string
+
+	actual, err := parseAWSInstances(awsResponse)
+
+	assert.Error(t, err)
+	assert.Equal(t, expected, actual)
+}
