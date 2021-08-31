a0d0c104fc0369313542e8a71ef26c4374eb869f
Grigorii Merkushev <brushknight@gmail.com>
2021-08-31 11:11:13 +0200

    wip: macos canary

--- tools/tools.mk
+++ tools/tools.mk
@@ -1,4 +1,26 @@
+.PHONY: install-deps
+install-deps:
+	cd $(CURDIR)/tools/spin-ec2
+	go mod download
+
+.PHONY: ec2-build
+ec2-build:
+	cd $(CURDIR)/tools/spin-ec2 \
+	 && mkdir -p bin \
+	 && go build -o bin/spin-ec2 *.go
+
 .PHONY: ec2
-ec2:
-	mkdir -p tools/spin-ec2/bin
-	go build -o tools/spin-ec2/bin/spin-ec2 tools/spin-ec2/*.go && tools/spin-ec2/bin/spin-ec2
+ec2: install-deps ec2-build
+	tools/spin-ec2/bin/spin-ec2
+
+.PHONY: canaries
+canaries: validate-aws-credentials install-deps ec2-build
+ifndef NR_LICENSE_KEY
+	@echo "NR_LICENSE_KEY variable must be provided for \"make canaries\""
+	exit 1
+endif
+ifndef VERSION
+	@echo "VERSION variable must be provided for \"make canaries\""
+	exit 1
+endif
+	tools/spin-ec2/bin/spin-ec2 canaries provision -v v$(VERSION) -l $(NR_LICENSE_KEY)
