c4206a6b1da831857761f885034bdfeddac29eb5
Grigorii Merkushev <brushknight@gmail.com>
2021-08-31 17:52:33 +0200

    wip: fix ec2 makefile

--- tools/tools.mk
+++ tools/tools.mk
@@ -1,4 +1,14 @@
+.PHONY: install-deps
+install-deps:
+	cd $(CURDIR)/tools/spin-ec2
+	go mod download
+
+.PHONY: ec2-build
+ec2-build:
+	cd $(CURDIR)/tools/spin-ec2 \
+	 && mkdir -p bin \
+	 && go build -o bin/spin-ec2 *.go
+
 .PHONY: ec2
-ec2:
-	mkdir -p tools/spin-ec2/bin
-	go build -o tools/spin-ec2/bin/spin-ec2 tools/spin-ec2/*.go && tools/spin-ec2/bin/spin-ec2
+ec2: install-deps ec2-build
+	tools/spin-ec2/bin/spin-ec2
