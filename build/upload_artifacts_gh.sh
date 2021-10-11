1e34e7f0bb7a37f9046601a30c712ed56c23c5ea
Ruben Ruiz de Gauna <rubenruizdegauna@gmail.com>
2021-10-11 14:49:48 +0200

    ci: add retries to asset upload to GH

--- build/upload_artifacts_gh.sh
+++ build/upload_artifacts_gh.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
-set -e
 #
 #
 # Upload dist artifacts to GH Release assets
 #
 #
+MAX_ATTEMPTS=5
+ATTEMPTS=$MAX_ATTEMPTS
 cd dist
 for filename in $(find . -name "*.msi" -o -name "*.rpm" -o -name "*.deb" -o -name "*.zip" -o -name "*.tar.gz");do
   echo "===> Uploading to GH $TAG: ${filename}"
-      gh release upload $TAG $filename
+  while [ $ATTEMPTS -gt 0 ];do
+    gh release upload --clobber $TAG $filename
+    if [[ $? -eq 0 ]];then
+      echo "===> uploaded  ${filename}"
+      break
+    fi
+    (( ATTEMPTS-- ))
+  done
+  if [ $ATTEMPTS -eq 0 ];then
+    echo "too many attempts to upload $filename"
+    exit 1
+  fi
+  ATTEMPTS=$MAX_ATTEMPTS
 done
