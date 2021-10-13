24dade63fa2ad621b3e820777731688290607e3c
cristianciutea <cristi.ciutea@gmail.com>
2021-10-13 11:10:03 +0200

    test: fix

--- build/upload_artifacts_gh.sh
+++ build/upload_artifacts_gh.sh
@@ -4,6 +4,37 @@
 # Upload dist artifacts to GH Release assets
 #
 #
+
+delete_asset_by_name() {
+  artifact="${1}"
+  repo="newrelic/infrastructure-agent"
+
+  assets_url=$(gh api "repos/${repo}/releases/tags/${TAG}" --jq '[.assets_url] | @tsv')
+  if [ "${?}" -ne 0 ]; then
+    exit 1
+  fi
+
+  assets=$(gh api "${assets_url}" --jq '.[] | [.url,.name] | @tsv' | tee)
+  if [ "${?}" -ne 0 ]; then
+    exit 2
+  fi
+
+  while IFS= read -r asset;
+  do
+    assetArray=($asset)
+    if [ "${assetArray[1]}" = "${artifact}"  ]; then
+      gh api -X DELETE "${assetArray[0]}"
+      if [ "${?}" -ne 0 ]; then
+        exit 3
+      fi
+      return
+    fi
+  done < <(echo "$assets")
+
+  echo "no assets found to delete with the name: ${artifact}"
+  exit 4
+}
+
 MAX_ATTEMPTS=5
 ATTEMPTS=$MAX_ATTEMPTS
 cd dist
@@ -15,6 +46,9 @@ for filename in $(find . -name "*.msi" -o -name "*.rpm" -o -name "*.deb" -o -nam
       echo "===> uploaded  ${filename}"
       break
     fi
+    set -e
+      delete_asset_by_name "${filename}"
+    set +e
     (( ATTEMPTS-- ))
   done
   if [ $ATTEMPTS -eq 0 ];then
