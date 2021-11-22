bc2dcced6d416594caf2dc18c64402ff22e90c5e
rubenruizdegauna <rubenruizdegauna@gmail.com>
2021-11-22 16:05:13 +0100

    Create canary alerts policies per os/distro. Added Makefile. Added to CI. (#833)

--- tools/tools.mk
+++ tools/tools.mk
@@ -1,43 +1,6 @@
-.PHONY: install-deps
-install-deps:
-	@echo "installing dependencies..."
-	@cd $(CURDIR)/tools/spin-ec2
-	@go mod download
 
-.PHONY: ec2-build
-ec2-build:
-	@echo "building tool..."
-	@cd $(CURDIR)/tools/spin-ec2 \
-	 && mkdir -p bin \
-	 && go build -o bin/spin-ec2 *.go
+include $(CURDIR)/tools/spin-ec2/spin-ec2.mk
+include $(CURDIR)/tools/provision-alerts/alerts.mk
 
-.PHONY: ec2
-ec2: install-deps ec2-build
-	@tools/spin-ec2/bin/spin-ec2
-
-.PHONY: canaries
-canaries: validate-aws-credentials install-deps ec2-build
-ifndef NR_LICENSE_KEY
-	@echo "NR_LICENSE_KEY variable must be provided for \"make canaries\""
-	exit 1
-endif
-ifndef VERSION
-	@echo "VERSION variable must be provided for \"make canaries\""
-	exit 1
-endif
-ifndef ANSIBLE_PASSWORD
-	@echo "ANSIBLE_PASSWORD variable must be provided for \"make canaries\""
-	exit 1
-endif
-	@read -p "Verify that you are in the VPN if needed and press any key to continue"
-	tools/spin-ec2/bin/spin-ec2 canaries provision -v 'v$(VERSION)' -l '$(NR_LICENSE_KEY)' -x '$(ANSIBLE_PASSWORD)'
-
-.PHONY: canaries-prune-dry
-canaries-prune-dry: validate-aws-credentials install-deps ec2-build
-	@read -p "DRY run for canaries prune, press enter to continue"
-	tools/spin-ec2/bin/spin-ec2 canaries prune --dry_run
-
-.PHONY: canaries-prune
-canaries-prune: validate-aws-credentials install-deps ec2-build
-	@read -p "REAL run for canaries prune, press enter to continue"
-	tools/spin-ec2/bin/spin-ec2 canaries prune
+.PHONY: tools-test
+tools-test: provision-alerts-tests
