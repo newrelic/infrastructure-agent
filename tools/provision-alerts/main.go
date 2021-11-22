e797d95e7d8d15f53d805368ff0eb572ef0c60e0
Grigorii Merkushev <brushknight@gmail.com>
2021-11-22 11:00:12 +0100

    Go 1.16 alerts tool (1) (#824)
    * canaries alerts creation tool
    
    * Added DeleteAll. More tests. Abstracted httpClient

---
+++ tools/provision-alerts/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"io/ioutil"
+	"log"
+	"net/http"
+	"provision-alerts/config"
+	"provision-alerts/infrastructure"
+	"provision-alerts/service"
+)
+
+func main() {
+
+	displayNameCurrent := "current"
+	displayNamePrevious := "current"
+
+	rawYAML, err := ioutil.ReadFile("template/template.yml")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	cfg, err := config.ParseConfig(rawYAML)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	cfg, err = config.FulfillConfig(cfg, displayNameCurrent, displayNamePrevious)
+
+	client := infrastructure.NewAlertClientHttp("https://staging-api.newrelic.com", "", &http.Client{})
+
+	policyService := service.NewPolicyApiService(client)
+
+	err = policyService.DeleteAll()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, policyConfig := range cfg.Policies {
+		policy, err := policyService.Create(policyConfig)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		for _, conditionConfig := range policyConfig.Conditions {
+			policy, err = policyService.AddCondition(policy, conditionConfig)
+
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+
+		for _, channelId := range policyConfig.Channels {
+
+			policy, err = policyService.AddChannel(policy, channelId)
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+	}
+}
