0234820177211cee8ce963139f4609cd76d1eb09
rubenruizdegauna <rubenruizdegauna@gmail.com>
2021-11-22 14:45:11 +0100

    Added input parameters. Added test for main (#826)

--- tools/provision-alerts/main.go
+++ tools/provision-alerts/main.go
@@ -1,38 +1,63 @@
 package main
 
 import (
+	"flag"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 	"provision-alerts/config"
 	"provision-alerts/infrastructure"
 	"provision-alerts/service"
 )
 
+var (
+	displayNameCurrent  string
+	displayNamePrevious string
+	apiKey              string
+	hostName            string
+)
+
 func main() {
 
-	displayNameCurrent := "current"
-	displayNamePrevious := "current"
+	flag.StringVar(&displayNameCurrent, "display_name_current", "", "display name of current version")
+	flag.StringVar(&displayNamePrevious, "display_name_previous", "", "display name of previous version")
+	flag.StringVar(&apiKey, "api_key", "", "NR api key")
+	flag.StringVar(&hostName, "host_name", "https://staging-api.newrelic.com", "NR api host (default staging)")
+	flag.Parse()
+	if !validArgs() {
+		flag.Usage()
+		os.Exit(1)
+	}
 
+	cfg, err := configFromTemplate()
+	if err != nil {
+		log.Fatal(err)
+	}
+	cfg, err = config.FulfillConfig(cfg, displayNameCurrent, displayNamePrevious)
+	client := infrastructure.NewAlertClientHttp(hostName, apiKey, &http.Client{})
+	policyService := service.NewPolicyApiService(client)
+
+	recreateAlerts(cfg, policyService)
+}
+
+func validArgs() bool {
+	return displayNameCurrent != "" && displayNamePrevious != "" && apiKey != ""
+}
+
+func configFromTemplate() (config.Config, error) {
 	rawYAML, err := ioutil.ReadFile("template/template.yml")
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	cfg, err := config.ParseConfig(rawYAML)
+	return config.ParseConfig(rawYAML)
+}
 
-	if err != nil {
-		log.Fatal(err)
-	}
+func recreateAlerts(cfg config.Config, policyService service.PolicyService) {
 
-	cfg, err = config.FulfillConfig(cfg, displayNameCurrent, displayNamePrevious)
-
-	client := infrastructure.NewAlertClientHttp("https://staging-api.newrelic.com", "", &http.Client{})
-
-	policyService := service.NewPolicyApiService(client)
-
-	err = policyService.DeleteAll()
+	err := policyService.DeleteAll()
 	if err != nil {
 		log.Fatal(err)
 	}
