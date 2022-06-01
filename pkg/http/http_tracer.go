6fcd76dffffbf2e063f75aed2f6fea472df637c9
rubenruizdegauna <rubenruizdegauna@gmail.com>
2022-06-01 16:26:38 +0200

    feat: log http requests traces (#1191)
    * feat: log http requests traces
    
    * moved to trace

---
+++ pkg/http/http_tracer.go
@@ -0,0 +1,96 @@
+package http
+
+import (
+	"crypto/tls"
+	"fmt"
+	wlog "github.com/newrelic/infrastructure-agent/pkg/log"
+	"net/http"
+	"net/http/httptrace"
+	"net/textproto"
+	"time"
+)
+
+var tlog = wlog.WithComponent("HttpTracer")
+
+func WithTracer(req *http.Request, requester string) *http.Request {
+	l := tlog.WithField("requester", requester)
+	traceStart := time.Now()
+	var getConnStart, dnsStart, conStart, tlsStart time.Time
+	trace := &httptrace.ClientTrace{
+		GetConn: func(hostPort string) {
+			getConnStart = time.Now()
+			l.WithField("action", "GetConn").
+				WithField("hostPort", hostPort).
+				Debug("")
+		},
+		GotConn: func(info httptrace.GotConnInfo) {
+			l.WithField("action", "GotConn").
+				WithField("wasIdle", info.WasIdle).
+				WithField("idleTime", info.IdleTime).
+				WithField("duration", fmt.Sprintf("%dms", time.Since(getConnStart).Milliseconds())).
+				Debug("")
+		},
+		GotFirstResponseByte: func() {
+			l.WithField("action", "GotFirstResponseByte").
+				WithField("duration", fmt.Sprintf("%dms", time.Since(traceStart).Milliseconds())).
+				Debug("")
+		},
+		Got100Continue: func() {
+			l.WithField("action", "Got100Continue").
+				Debug("")
+		},
+		Got1xxResponse: func(code int, header textproto.MIMEHeader) error {
+			l.WithField("action", "Got1xxResponse").
+				WithField("code", code).Debug("")
+
+			return nil
+		},
+		DNSStart: func(info httptrace.DNSStartInfo) {
+			dnsStart = time.Now()
+			l.WithField("action", "DNSStart").
+				Debug("")
+
+		},
+		DNSDone: func(info httptrace.DNSDoneInfo) {
+			l.WithField("action", "DNSDone").
+				WithField("duration", fmt.Sprintf("%dms", time.Since(dnsStart).Milliseconds())).
+				Debug("")
+		},
+		ConnectStart: func(network, addr string) {
+			conStart = time.Now()
+			l.WithField("action", "ConnectStart").
+				WithField("network", network).
+				WithField("addr", addr).
+				Debug("")
+		},
+		ConnectDone: func(network, addr string, err error) {
+			l.WithField("action", "ConnectDone").
+				WithField("network", network).
+				WithField("addr", addr).
+				WithError(err).
+				WithField("duration", fmt.Sprintf("%dms", time.Since(conStart).Milliseconds())).
+				Debug("")
+		},
+		TLSHandshakeStart: func() {
+			tlsStart = time.Now()
+			l.WithField("action", "TLSHandshakeStart").
+				Debug("")
+		},
+		TLSHandshakeDone: func(tls.ConnectionState, error) {
+			l.WithField("action", "TLSHandshakeDone").
+				WithField("duration", fmt.Sprintf("%dms", time.Since(tlsStart).Milliseconds())).
+				Debug("")
+		},
+		WroteHeaders: func() {
+			l.WithField("action", "WroteHeaders").
+				Debug("")
+		},
+		WroteRequest: func(info httptrace.WroteRequestInfo) {
+			l.WithField("action", "WroteRequest").
+				WithError(info.Err).
+				Debug("")
+		},
+	}
+
+	return req.WithContext(httptrace.WithClientTrace(req.Context(), trace))
+}
