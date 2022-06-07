6da929612feec59a0fbc99fe18be5674de6e1484
Grigorii Merkushev <brushknight@gmail.com>
2022-06-07 17:37:51 +0200

    fix git fetc

--- build/ci/entrypoint.sh
+++ build/ci/entrypoint.sh
@@ -3,5 +3,6 @@ target=$1
 
 echo "$SSH_KEY" | base64 --decode > ~/.ssh/caos-dev-arm.cer
 chmod 600  ~/.ssh/caos-dev-arm.cer
+git fetch origin
 git checkout $REF
 ANSIBLE_INVENTORY=$ANSIBLE_INVENTORY make $target
