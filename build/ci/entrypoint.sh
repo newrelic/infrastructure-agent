53e8e4cb17e1734c5da5f125f4f43ad95f1cdd5a
Grigorii Merkushev <brushknight@gmail.com>
2022-06-07 18:19:13 +0200

    fix typo

--- build/ci/entrypoint.sh
+++ build/ci/entrypoint.sh
@@ -5,5 +5,5 @@ echo "$SSH_KEY" | base64 --decode > ~/.ssh/caos-dev-arm.cer
 chmod 600  ~/.ssh/caos-dev-arm.cer
 git fetch origin
 git checkout $REF
-mkdir -p $ANSIBLE_INVENTORY_FOLDER_ARG
+mkdir -p $ANSIBLE_INVENTORY_FOLDER
 ANSIBLE_INVENTORY=$ANSIBLE_INVENTORY make $target
