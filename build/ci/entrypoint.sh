5be1a7abf0e9a104b9b742db1a955fc4f845ca95
Roger Coll <rogercoll@protonmail.com>
2022-06-23 10:50:05 +0200

    feat: restore make as entrypoint

--- build/ci/entrypoint.sh
+++ build/ci/entrypoint.sh
@@ -6,4 +6,4 @@ chmod 600  ~/.ssh/caos-dev-arm.cer
 git fetch origin
 git checkout $REF
 mkdir -p $ANSIBLE_INVENTORY_FOLDER
-ANSIBLE_INVENTORY=$ANSIBLE_INVENTORY $target
+ANSIBLE_INVENTORY=$ANSIBLE_INVENTORY make $target
