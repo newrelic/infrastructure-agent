186d26ff47f48f91833fdf75598ca333e77c6efe
Cristian Ciutea <cristi.ciutea@gmail.com>
2022-07-01 15:25:21 +0200

    feat: added log rotation mechanism

---
+++ pkg/log/rotate.go
@@ -0,0 +1,183 @@
+// Copyright 2022 New Relic Corporation. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package log
+
+import (
+	"errors"
+	"fmt"
+	"github.com/newrelic/infrastructure-agent/pkg/disk"
+	"os"
+	"path/filepath"
+	"strings"
+	"sync"
+	"time"
+)
+
+// defaultDatePattern used to generate filename for the rotated file.
+const defaultDatePattern = "YYYY-MM-DD_hh-mm-ss"
+
+var (
+	// ErrFileNotOpened is returned when an operation cannot be performed because the file is not opened.
+	ErrFileNotOpened = errors.New("cannot perform operation, file is not opened")
+)
+
+// FileWithRotationConfig keeps the configuration for a new FileWithRotation.
+type FileWithRotationConfig struct {
+	File            string
+	FileNamePattern string
+	MaxSizeInBytes  int64
+}
+
+// FileWithRotation decorates a file with rotation mechanism.
+type FileWithRotation struct {
+	sync.Mutex
+	cfg FileWithRotationConfig
+
+	file         *os.File
+	writtenBytes int64
+
+	getTimeFn func() time.Time
+}
+
+// NewFileWithRotation creates a new FileWithRotation.
+func NewFileWithRotation(cfg FileWithRotationConfig) *FileWithRotation {
+	return &FileWithRotation{
+		cfg:       cfg,
+		getTimeFn: time.Now,
+	}
+}
+
+// Open the file to write in. If the file doesn't exist, a new file will be created.
+func (f *FileWithRotation) Open() (*FileWithRotation, error) {
+	f.Lock()
+	defer f.Unlock()
+
+	return f, f.open()
+}
+
+// Close the file.
+func (f *FileWithRotation) Close() error {
+	f.Lock()
+	defer f.Unlock()
+
+	if f.file == nil {
+		return ErrFileNotOpened
+	}
+
+	return f.file.Close()
+}
+
+// Write will check if the new content can be written into the file. If not, the file will be
+// automatically rotated.
+func (f *FileWithRotation) Write(b []byte) (n int, err error) {
+	f.Lock()
+	defer f.Unlock()
+
+	newContentSize := int64(len(b))
+
+	// Make sure new content fits the max size from the configuration.
+	if newContentSize > f.cfg.MaxSizeInBytes {
+		return 0, fmt.Errorf("failed to write to file, new content size: '%db' exceeds to maximum file size: '%db'",
+			newContentSize, f.cfg.MaxSizeInBytes)
+	}
+
+	// Check if the file should be rotated.
+	if f.cfg.MaxSizeInBytes > 0 && f.writtenBytes+newContentSize > f.cfg.MaxSizeInBytes {
+		err = f.rotate()
+		if err != nil {
+			return
+		}
+	}
+
+	if f.file == nil {
+		return 0, ErrFileNotOpened
+	}
+
+	writtenBytes, err := f.file.Write(b)
+	f.writtenBytes += int64(writtenBytes)
+
+	return writtenBytes, err
+}
+
+func (f *FileWithRotation) open() error {
+	var err error
+	f.file, err = disk.OpenFile(f.cfg.File, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		return fmt.Errorf("failed to open file rotate, error: %v", err)
+	}
+
+	if f.file == nil {
+		return ErrFileNotOpened
+	}
+
+	fileStat, err := f.file.Stat()
+	if err != nil {
+		return fmt.Errorf("failed to open file rotate, error while reading file stat: %v", err)
+	}
+
+	f.writtenBytes = fileStat.Size()
+	return nil
+}
+
+// rotate will rename the current file according to the filename pattern and will open a new file.
+func (f *FileWithRotation) rotate() error {
+	if f.file == nil {
+		return ErrFileNotOpened
+	}
+
+	if err := f.file.Close(); err != nil {
+		return fmt.Errorf("failed to rotate file, error while closing the current file: %v", err)
+	}
+
+	// Generate the rotation filename according to the config.
+	dir := filepath.Dir(f.cfg.File)
+	rotateFileName := filepath.Join(dir, f.generateFileName())
+
+	if err := os.Rename(f.cfg.File, rotateFileName); err != nil {
+		return fmt.Errorf("failed to rotate file, error while moving the current file: %v", err)
+	}
+
+	if err := f.open(); err != nil {
+		return fmt.Errorf("failed to create new file after rotation, error: %v", err)
+	}
+
+	return nil
+}
+
+// generateFileName will use the specified pattern to create a new filename when the current file is rotated.
+// If the pattern is not specified in the configuration, by default a new filename will be created with
+// the following pattern: current_file_name_defaultDatePattern.current_file_extension.
+func (f *FileWithRotation) generateFileName() string {
+
+	pattern := f.cfg.FileNamePattern
+
+	// If a custom pattern for the rotated filename wasn't provided, generated one.
+	if pattern == "" {
+		// Insert time into the log filename.
+		ext := filepath.Ext(f.cfg.File)
+		fileName := filepath.Base(f.cfg.File)
+		fileName = strings.TrimSuffix(fileName, ext)
+
+		pattern = fmt.Sprintf("%s_%s%s", fileName, defaultDatePattern, ext)
+	}
+
+	return formatTime(pattern, f.getTimeFn())
+}
+
+// formatTime will receive a time object and a pattern to format the current time.
+func formatTime(pattern string, ts time.Time) string {
+	tokens := map[string]string{
+		"YYYY": fmt.Sprintf("%d", ts.Year()),
+		"MM":   fmt.Sprintf("%02d", ts.Month()),
+		"DD":   fmt.Sprintf("%02d", ts.Day()),
+		"hh":   fmt.Sprintf("%02d", ts.Hour()),
+		"mm":   fmt.Sprintf("%02d", ts.Minute()),
+		"ss":   fmt.Sprintf("%02d", ts.Second()),
+	}
+
+	for token, replacer := range tokens {
+		pattern = strings.Replace(pattern, token, replacer, -1)
+	}
+	return pattern
+}
---
+++ pkg/log/rotate_test.go
@@ -0,0 +1,337 @@
+// Copyright 2022 New Relic Corporation. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package log
+
+import (
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"testing"
+	"time"
+)
+
+func TestFormatTime(t *testing.T) {
+	date := time.Date(2022, time.January, 1, 01, 23, 45, 0, time.Local)
+
+	testCases := []struct {
+		name     string
+		pattern  string
+		expected string
+	}{
+		{
+			name:     "TokensAreReplaced",
+			pattern:  "YYYY/MM/DD-hh:mm:ss",
+			expected: "2022/01/01-01:23:45",
+		},
+		{
+			name:     "MultipleReplacements",
+			pattern:  "YYYY YYYY/MM MM/DD DD-hh hh:mm mm:ss ss",
+			expected: "2022 2022/01 01/01 01-01 01:23 23:45 45",
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			formattedTime := formatTime(testCase.pattern, date)
+			assert.Equal(t, testCase.expected, formattedTime)
+		})
+	}
+}
+
+func TestGenerateFileName(t *testing.T) {
+	date := time.Date(2022, time.January, 1, 01, 23, 45, 0, time.Local)
+
+	testCases := []struct {
+		name     string
+		config   FileWithRotationConfig
+		expected string
+	}{
+		{
+			name: "FileWithDate",
+			config: FileWithRotationConfig{
+				File: "newrelic-infra.log",
+			},
+			expected: "newrelic-infra_2022-01-01_01-23-45.log",
+		},
+		{
+			name: "FileWithPathAndDate",
+			config: FileWithRotationConfig{
+				File: "/var/log/newrelic-infra/newrelic-infra.log",
+			},
+			expected: "newrelic-infra_2022-01-01_01-23-45.log",
+		},
+		{
+			name: "FileWithTokensInPath",
+			config: FileWithRotationConfig{
+				File: "/var/log/newrelic-infraYYYYMMDDhhmmss/newrelic-infra.log",
+			},
+			expected: "newrelic-infra_2022-01-01_01-23-45.log",
+		},
+		{
+			name: "FileWithTokensInExtension",
+			config: FileWithRotationConfig{
+				File: "/var/log/newrelic-infra/newrelic-infra.logYYYYMMDDhhmmss",
+			},
+			expected: "newrelic-infra_2022-01-01_01-23-45.log20220101012345",
+		},
+		{
+			name: "CustomPattern",
+			config: FileWithRotationConfig{
+				File:            "/var/log/newrelic-infra/newrelic-infra.log",
+				FileNamePattern: "xyz_YYYY:DD:MM:hh:mm:ss",
+			},
+			expected: "xyz_2022:01:01:01:23:45",
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			f := FileWithRotation{
+				cfg: testCase.config,
+				getTimeFn: func() time.Time {
+					return date
+				},
+			}
+			fileName := formatTime(f.generateFileName(), date)
+			assert.Equal(t, testCase.expected, fileName)
+		})
+	}
+}
+
+func TestOpenFileWithRotation(t *testing.T) {
+	logFile := filepath.Join(os.TempDir(), "newrelic-infra.log")
+	cfg := FileWithRotationConfig{
+		File:            logFile,
+		FileNamePattern: "",
+	}
+
+	// GIVEN a new NewFileWithRotation
+	file, err := NewFileWithRotation(cfg).Open()
+
+	defer func() {
+		assert.NoError(t, file.Close())
+		assert.NoError(t, os.Remove(logFile))
+	}()
+
+	require.NoError(t, err)
+
+	// File can be opened
+	_, err = os.Stat(logFile)
+	require.NoError(t, err)
+}
+
+func TestNewContentFitsMaxSizeInBytes(t *testing.T) {
+	logFile := filepath.Join(os.TempDir(), "newrelic-infra.log")
+
+	// GIVEN a new NewFileWithRotation
+	cfg := FileWithRotationConfig{
+		File:            logFile,
+		MaxSizeInBytes:  1,
+		FileNamePattern: "",
+	}
+
+	file, err := NewFileWithRotation(cfg).Open()
+
+	defer func() {
+		assert.NoError(t, file.Close())
+		assert.NoError(t, os.Remove(logFile))
+	}()
+
+	require.NoError(t, err)
+
+	// WHEN writing a content that exceeds the maxSize config
+	n, err := file.Write([]byte{1, 2})
+
+	// THEN error is returned
+	assert.Equal(t, n, 0)
+	assert.EqualError(t, err, "failed to write to file, new content size: '2b' exceeds to maximum file size: '1b'")
+
+	// WHEN writing a message that fits the maxSize
+	n, err = file.Write([]byte{1})
+
+	// THEN no error is returned
+	assert.Equal(t, n, 1)
+	assert.NoError(t, err)
+}
+
+func TestFileRotate(t *testing.T) {
+	tmp := os.TempDir()
+	logFile := filepath.Join(tmp, "newrelic-infra.log")
+	rotatedLogFile := filepath.Join(tmp, "newrelic-infra_2022-01-01_01-23-45.log")
+
+	// Make sure files don't exist.
+	os.Remove(logFile)
+	os.Remove(rotatedLogFile)
+
+	// GIVEN a new NewFileWithRotation
+	cfg := FileWithRotationConfig{
+		File:            logFile,
+		MaxSizeInBytes:  1,
+		FileNamePattern: "",
+	}
+
+	file, err := NewFileWithRotation(cfg).Open()
+
+	defer func() {
+		assert.NoError(t, file.Close())
+
+		assert.NoError(t, os.Remove(logFile))
+		assert.NoError(t, os.Remove(rotatedLogFile))
+	}()
+
+	require.NoError(t, err)
+
+	// Mock the date for filename rename
+	file.getTimeFn = func() time.Time {
+		return time.Date(2022, time.January, 1, 01, 23, 45, 0, time.Local)
+	}
+
+	content := []byte{1}
+
+	// WHEN writing a message that doesn't exceed the limit
+	n, err := file.Write(content)
+
+	// THEN no error is returned
+	assert.Equal(t, n, 1)
+	assert.NoError(t, err)
+
+	// WHEN writing another message when max size already reached
+	n, err = file.Write(content)
+
+	// THEN no error is returned
+	assert.Equal(t, n, 1)
+	assert.NoError(t, err)
+
+	// AND file was rotated
+	_, err = os.Stat(rotatedLogFile)
+	require.NoError(t, err)
+
+	// AND content from both file matches expectations
+	b, err := ioutil.ReadFile(logFile)
+	assert.NoError(t, err)
+	assert.Equal(t, content, b)
+
+	b, err = ioutil.ReadFile(rotatedLogFile)
+	assert.NoError(t, err)
+	assert.Equal(t, content, b)
+
+	// AND written bytes counter was reset.
+	assert.Equal(t, file.writtenBytes, int64(1))
+}
+
+func TestCloseAlreadyClosedFile(t *testing.T) {
+	tmp := os.TempDir()
+	logFile := filepath.Join(tmp, "newrelic-infra.log")
+
+	// Make sure files don't exist.
+	os.Remove(logFile)
+
+	// GIVEN a new NewFileWithRotation
+	cfg := FileWithRotationConfig{
+		File:            logFile,
+		MaxSizeInBytes:  1,
+		FileNamePattern: "",
+	}
+
+	file, err := NewFileWithRotation(cfg).Open()
+
+	defer func() {
+		assert.NoError(t, os.Remove(logFile))
+	}()
+
+	require.NoError(t, err)
+
+	// THEN no error on 1st close call
+	err = file.Close()
+	assert.NoError(t, err)
+
+	// AND error is returned on 2nd close call
+	err = file.Close()
+	assert.Error(t, err)
+}
+
+func TestWrite(t *testing.T) {
+	tmp := os.TempDir()
+	logFile := filepath.Join(tmp, "newrelic-infra.log")
+
+	// Make sure files don't exist.
+	os.Remove(logFile)
+
+	// GIVEN a new NewFileWithRotation
+	cfg := FileWithRotationConfig{
+		File:            logFile,
+		MaxSizeInBytes:  100,
+		FileNamePattern: "",
+	}
+
+	file, err := NewFileWithRotation(cfg).Open()
+
+	defer func() {
+		assert.NoError(t, file.Close())
+		assert.NoError(t, os.Remove(logFile))
+	}()
+
+	require.NoError(t, err)
+
+	// WHEN writing a message
+	written1, err := file.Write([]byte("message1"))
+
+	// THEN no error
+	require.NoError(t, err)
+
+	// WHEN closing the file and writing a message
+	err = file.Close()
+	require.NoError(t, err)
+
+	written, err := file.Write([]byte("something else"))
+
+	// THEN error is returned
+	require.Equal(t, written, 0)
+	require.Error(t, err)
+
+	// WHEN reopening the file
+	_, err = file.Open()
+	require.NoError(t, err)
+
+	// THEN no error is returned
+	written2, err := file.Write([]byte("message2"))
+	require.NoError(t, err)
+
+	// AND writtenBytes is increased
+	assert.Equal(t, file.writtenBytes, int64(written1+written2))
+
+	// AND the content matches the expectation
+	b, err := ioutil.ReadFile(logFile)
+	assert.NoError(t, err)
+	assert.Equal(t, []byte("message1message2"), b)
+}
+
+func TestWhenFileNotOpen(t *testing.T) {
+	tmp := os.TempDir()
+	logFile := filepath.Join(tmp, "newrelic-infra.log")
+
+	// GIVEN a new NewFileWithRotation
+	cfg := FileWithRotationConfig{
+		File:            logFile,
+		MaxSizeInBytes:  100,
+		FileNamePattern: "",
+	}
+
+	file := NewFileWithRotation(cfg)
+
+	// WHEN writing to a file that is not opened
+	n, err := file.Write([]byte("message1"))
+
+	// THEN error is returned
+	assert.Equal(t, n, 0)
+	require.ErrorIs(t, err, ErrFileNotOpened)
+
+	// WHEN closing the file
+	err = file.Close()
+
+	// Error is returned
+	require.ErrorIs(t, err, ErrFileNotOpened)
+}
