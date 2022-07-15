2026f2c1013e4b8235b0c6efc8e56a44440cf848
Grigorii Merkushev <brushknight@gmail.com>
2022-07-15 18:18:05 +0200

    ci: canaries & packaging tests in pre-release

--- test/test.mk
+++ test/test.mk
@@ -1,15 +1,26 @@
 PROVISION_HOST_PREFIX := $(shell whoami)-$(shell hostname)
 AWS_ACCOUNT_ID = "018789649883"# CAOS
 
-ANSIBLE_INVENTORY ?= $(CURDIR)/test/automated/ansible/inventory.ec2
+ANSIBLE_INVENTORY_FOLDER ?= $(CURDIR)/test/automated/ansible
+
+ifeq ($(origin ANSIBLE_INVENTORY_FILE), undefined)
+  ANSIBLE_INVENTORY = $(CURDIR)/test/automated/ansible/inventory.ec2
+else
+  ANSIBLE_INVENTORY = $(ANSIBLE_INVENTORY_FOLDER)/$(ANSIBLE_INVENTORY_FILE)
+endif
 
 .PHONY: test/automated/provision
 test/automated/provision: validate-aws-credentials
+	echo "inventory $(ANSIBLE_INVENTORY)"
+ifndef PLATFORM
+	@echo "PLATFORM variable must be provided for test/automated/provision"
+	exit 1
+endif
 ifndef ANSIBLE_PASSWORD_WINDOWS
 	@echo "ANSIBLE_PASSWORD_WINDOWS variable must be provided for test/automated/provision"
 	exit 1
 endif
-	PROVISION_HOST_PREFIX=$(PROVISION_HOST_PREFIX) $(CURDIR)/test/automated/ansible/provision.sh
+	PROVISION_HOST_PREFIX=$(PROVISION_HOST_PREFIX) PLATFORM=$(PLATFORM) ANSIBLE_INVENTORY=$(ANSIBLE_INVENTORY) $(CURDIR)/test/automated/ansible/provision.sh
 
 .PHONY: test/automated/termination
 test/automated/termination: validate-aws-credentials
