c45bd63c293b9ed8dcf21de1cf8df2e38ecc14e9
Roger Coll <rogercoll@protonmail.com>
2022-07-21 14:38:58 +0200

    feat: add labels to agent config protocol (#1322)
    * feat: add labels to agent config protocol
    
    * feat: add tests for config protocol labels
    
    * feat: init map outside the loop

--- pkg/integrations/configrequest/handler.go
+++ pkg/integrations/configrequest/handler.go
@@ -44,6 +44,19 @@ func NewHandleFn(configProtocolQueue chan<- Entry, terminateDefinitionQueue chan
 				logger.WithError(err).WithFields(logCtx).Warn(logFailedConfigTemplate)
 				return
 			}
+
+			// Add parent labels.
+			if ce.Labels == nil && len(parentDefinition.Labels) > 0 {
+				ce.Labels = make(map[string]string, len(parentDefinition.Labels))
+			}
+
+			for key, val := range parentDefinition.Labels {
+				// Do not overwrite tags received from config protocol.
+				if _, found := ce.Labels[key]; !found {
+					ce.Labels[key] = val
+				}
+			}
+
 			def, err := integration.NewDefinition(ce, il, parentDefinition.ExecutorConfig.Passthrough, template)
 			if err != nil {
 				logger.WithError(err).WithFields(logCtx).Warn(logFailedDefinition)
