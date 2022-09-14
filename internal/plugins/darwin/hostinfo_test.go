a271a9bf4247099a9c8ced98ade58d083cfca0fe
Roger Coll <rogercoll@protonmail.com>
2022-09-14 14:17:53 +0200

    feat: parse macos arm host information

--- internal/plugins/darwin/hostinfo_test.go
+++ internal/plugins/darwin/hostinfo_test.go
@@ -6,6 +6,8 @@
 package darwin
 
 import (
+	"testing"
+
 	"github.com/newrelic/infrastructure-agent/internal/agent"
 	testing2 "github.com/newrelic/infrastructure-agent/internal/plugins/testing"
 	"github.com/newrelic/infrastructure-agent/pkg/plugins/ids"
@@ -13,7 +15,6 @@ import (
 	"github.com/pkg/errors"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
-	"testing"
 )
 
 func TestGetKernelRelease_Success(t *testing.T) {
@@ -122,17 +123,81 @@ func TestValidateHardwareUUID(t *testing.T) {
 	}
 }
 
-func TestData(t *testing.T) {
-	hip := HostinfoPlugin{
-		PluginCommon: agent.PluginCommon{
-			ID:      ids.HostInfo,
-			Context: testing2.NewMockAgent(),
+func TestSplitRightSubstring(t *testing.T) {
+	var testCases = []struct {
+		name      string
+		output    string
+		substring string
+		separator string
+		expected  string
+	}{
+		{name: "Empty input",
+			output:    "",
+			substring: "",
+			separator: "",
+			expected:  "",
 		},
+		{name: "Empty substring",
+			output:    "Hello bye yes",
+			substring: "",
+			separator: "$",
+			expected:  "",
+		},
+		{name: "Empty separator",
+			output:    "Hello bye yes",
+			substring: "bye",
+			separator: "",
+			expected:  "",
+		},
+		{name: "Word separator",
+			output:    "Hello bye yes",
+			substring: "Hello ",
+			separator: " yes",
+			expected:  "bye",
+		},
+		{name: "Dot separator",
+			output:    "Fosdem: A lot of questions.",
+			substring: "Fosdem: ",
+			separator: ".",
+			expected:  "A lot of questions",
+		},
+		{name: "Newline separator",
+			output: `Fosdem: A lot of questions
+`,
+			substring: "Fosdem: ",
+			separator: "\n",
+			expected:  "A lot of questions",
+		},
+		{name: "Substring high slice bound",
+			output:    "Fosdem: A lot of questions",
+			substring: "Fosdem: A lot of questions",
+			separator: "",
+			expected:  "",
+		},
+	}
+	for _, tt := range testCases {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equal(
+				t,
+				splitRightSubstring(tt.output,
+					tt.substring,
+					tt.separator,
+				),
+				tt.expected)
+		})
+	}
+}
 
-		cloudHarvester: cloud.NewDetector(true, 0, 0, 0, true),
-		readDataFromCmd: func(cmd string, args ...string) (string, error) {
-			if cmd == "system_profiler" {
-				return `Hardware:
+func TestData(t *testing.T) {
+	var testCases = []struct {
+		name                 string
+		unameOutput          string
+		systemProfilerOutput string
+		expectedData         HostInfoData
+	}{
+		{name: "Intel arch output",
+			unameOutput: "Darwin Kernel Version 20.3.0: Thu Jan 21 00:07:06 PST 2021; root:xnu-7195.81.3~1/RELEASE_X86_64`",
+			systemProfilerOutput: `Hardware:
 
     Hardware Overview:
 
@@ -149,52 +214,102 @@ func TestData(t *testing.T) {
       System Firmware Version: 1554.80.3.0.0 (iBridge: 18.16.14347.0.0,0)
       Serial Number (system): abcd
       Hardware UUID: C3805006-DFCF-11EB-BA80-0242AC130004
-      Provisioning UDID: C3805006-DFCF-11EB-BA80-0242AC130004
-`, nil
-			} else if cmd == "uname" {
-				return `Darwin Kernel Version 20.3.0: Thu Jan 21 00:07:06 PST 2021; root:xnu-7195.81.3~1/RELEASE_X86_64`, nil
-			}
-			return ``, errors.New("unknown command")
+`,
+			expectedData: HostInfoData{
+				System:          "system",
+				Distro:          "macOS 11.2.3",
+				KernelVersion:   "20.3.0",
+				HostType:        "MacBook Pro MacBookPro15,1",
+				CpuName:         "6-Core Intel Core i7 @ 2.6 GHz",
+				CpuNum:          "6",
+				TotalCpu:        "6",
+				Ram:             "16777216 kB",
+				UpSince:         "2021-07-01 09:59:30",
+				AgentVersion:    "mock",
+				AgentName:       "Infrastructure",
+				AgentMode:       "root",
+				OperatingSystem: "macOS",
+				ProductUuid:     "C3805006-DFCF-11EB-BA80-0242AC130004",
+			},
 		},
+		{name: "Arm arch output",
+			unameOutput: "Darwin Kernel Version 21.6.0: Wed Aug 10 14:28:23 PDT 2022; root:xnu-8020.141.5~2/RELEASE_ARM64_T6000",
+			systemProfilerOutput: `Hardware:
+
+    Hardware overview:
+
+      Model Name: MacBook Pro
+      Model Identifier: MacBookPro18,4
+      Chip: Apple M1 Max
+      Total Number of Cores: 10 (8 performance and 2 efficiency)
+      Memory: 64 GB
+      System Firmware Version: 7459.141.1
+      OS Loader Version: 7459.141.1
+      Serial Number (system): H9AAAAA9WK
+      Hardware UUID: E62094F3-9A33-5555-5555-F4C3A1E16AC5
+      Provisioning UDID: 00006000-000000000C1A801E
+      Activation Lock Status: Disabled
+`,
+			expectedData: HostInfoData{
+				System:          "system",
+				Distro:          "macOS 11.2.3",
+				KernelVersion:   "21.6.0",
+				HostType:        "MacBook Pro MacBookPro18,4",
+				CpuName:         "Apple M1 Max",
+				CpuNum:          "10",
+				TotalCpu:        "10",
+				Ram:             "67108864 kB",
+				UpSince:         "2021-07-01 09:59:30",
+				AgentVersion:    "mock",
+				AgentName:       "Infrastructure",
+				AgentMode:       "root",
+				OperatingSystem: "macOS",
+				ProductUuid:     "E62094F3-9A33-5555-5555-F4C3A1E16AC5",
+			},
+		},
+	}
+
+	hip := HostinfoPlugin{
+		PluginCommon: agent.PluginCommon{
+			ID:      ids.HostInfo,
+			Context: testing2.NewMockAgent(),
+		},
+		cloudHarvester: cloud.NewDetector(true, 0, 0, 0, true),
 	}
 	hip.Context.Config().DisableCloudMetadata = true
 	hip.Context.Config().RunMode = "root"
 
-	expected := &HostInfoData{
-		System:          "system",
-		Distro:          "macOS 11.2.3",
-		KernelVersion:   "20.3.0",
-		HostType:        "MacBook Pro MacBookPro15,1",
-		CpuName:         "6-Core Intel Core i7 @ 2.6 GHz",
-		CpuNum:          "6",
-		TotalCpu:        "6",
-		Ram:             "16777216 kB",
-		UpSince:         "2021-07-01 09:59:30",
-		AgentVersion:    "mock",
-		AgentName:       "Infrastructure",
-		AgentMode:       "root",
-		OperatingSystem: "macOS",
-		ProductUuid:     "C3805006-DFCF-11EB-BA80-0242AC130004",
+	for _, tt := range testCases {
+		t.Run(tt.name, func(t *testing.T) {
+			hip.readDataFromCmd = func(cmd string, args ...string) (string, error) {
+				if cmd == "system_profiler" {
+					return tt.systemProfilerOutput, nil
+				} else if cmd == "uname" {
+					return tt.unameOutput, nil
+				}
+				return ``, errors.New("unknown command")
+			}
+			//Some values (distro, upSince) are being read from the host, so commented until fix those (out of scope in this task)
+			//assert.Equal(t, expected, hip.Data()[0])
+			data := hip.Data()[0].(*HostInfoData)
+
+			assert.Equal(t, tt.expectedData.System, data.System)
+			//assert.Equal(t, expected.Distro, data.Distro)
+			assert.Equal(t, tt.expectedData.KernelVersion, data.KernelVersion)
+			assert.Equal(t, tt.expectedData.HostType, data.HostType)
+			assert.Equal(t, tt.expectedData.CpuName, data.CpuName)
+			assert.Equal(t, tt.expectedData.CpuNum, data.CpuNum)
+			assert.Equal(t, tt.expectedData.TotalCpu, data.TotalCpu)
+			assert.Equal(t, tt.expectedData.Ram, data.Ram)
+			//assert.Equal(t, expected.UpSince, data.UpSince)
+			assert.Equal(t, tt.expectedData.AgentVersion, data.AgentVersion)
+			assert.Equal(t, tt.expectedData.AgentName, data.AgentName)
+			assert.Equal(t, tt.expectedData.AgentMode, data.AgentMode)
+			assert.Equal(t, tt.expectedData.OperatingSystem, data.OperatingSystem)
+			assert.Equal(t, tt.expectedData.ProductUuid, data.ProductUuid)
+		})
 	}
 
-	//Some values (distro, upSince) are being read from the host, so commented until fix those (out of scope in this task)
-	//assert.Equal(t, expected, hip.Data()[0])
-	data := hip.Data()[0].(*HostInfoData)
-
-	assert.Equal(t, expected.System, data.System)
-	//assert.Equal(t, expected.Distro, data.Distro)
-	assert.Equal(t, expected.KernelVersion, data.KernelVersion)
-	assert.Equal(t, expected.HostType, data.HostType)
-	assert.Equal(t, expected.CpuName, data.CpuName)
-	assert.Equal(t, expected.CpuNum, data.CpuNum)
-	assert.Equal(t, expected.TotalCpu, data.TotalCpu)
-	assert.Equal(t, expected.Ram, data.Ram)
-	//assert.Equal(t, expected.UpSince, data.UpSince)
-	assert.Equal(t, expected.AgentVersion, data.AgentVersion)
-	assert.Equal(t, expected.AgentName, data.AgentName)
-	assert.Equal(t, expected.AgentMode, data.AgentMode)
-	assert.Equal(t, expected.OperatingSystem, data.OperatingSystem)
-	assert.Equal(t, expected.ProductUuid, data.ProductUuid)
 }
 
 type fakeHarvester struct {
