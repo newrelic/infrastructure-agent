8cc898c88f1e99e03d4fa7ce24362e5710e26b06
Ruben Ruiz de Gauna <rubenruizdegauna@gmail.com>
2022-10-04 17:37:14 +0200

    NEWRELIC-4147 fix: don't try to reach local servers on error

--- internal/httpapi/httpapi_test.go
+++ internal/httpapi/httpapi_test.go
@@ -10,8 +10,10 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"net"
 	"net/http"
 	"net/http/httptest"
+	"sort"
 	"testing"
 	"time"
 
@@ -19,9 +21,11 @@ import (
 	"github.com/newrelic/infrastructure-agent/internal/agent/status"
 	"github.com/newrelic/infrastructure-agent/internal/integrations/v4/testhelp/testemit"
 	"github.com/newrelic/infrastructure-agent/pkg/entity"
-	network_helpers "github.com/newrelic/infrastructure-agent/pkg/helpers/network"
+	networkHelpers "github.com/newrelic/infrastructure-agent/pkg/helpers/network"
 	"github.com/newrelic/infrastructure-agent/pkg/integrations/v4/fixtures"
 	"github.com/newrelic/infrastructure-agent/pkg/log"
+	logHelper "github.com/newrelic/infrastructure-agent/test/log"
+	"github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
@@ -30,7 +34,7 @@ func TestServe_Status(t *testing.T) {
 	t.Parallel()
 
 	// Given a running HTTP endpoint
-	port, err := network_helpers.TCPPort()
+	port, err := networkHelpers.TCPPort()
 	require.NoError(t, err)
 
 	serverOk := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -59,7 +63,7 @@ func TestServe_Status(t *testing.T) {
 
 	go s.Serve(ctx)
 
-	s.WaitUntilReady()
+	s.waitUntilReady()
 
 	// And a request to the status API is sent
 	req, err := http.NewRequest(http.MethodGet, fmt.Sprintf("http://localhost:%d%s", port, statusAPIPath), nil)
@@ -86,7 +90,7 @@ func TestServe_OnlyErrors(t *testing.T) {
 	t.Parallel()
 
 	// Given a running HTTP endpoint and an errored one (which times out)
-	port, err := network_helpers.TCPPort()
+	port, err := networkHelpers.TCPPort()
 	require.NoError(t, err)
 
 	serverOk := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -116,11 +120,10 @@ func TestServe_OnlyErrors(t *testing.T) {
 	s, err := NewServer(r, em)
 	require.NoError(t, err)
 	s.Status.Enable("localhost", port)
-	defer cancel()
 
 	go s.Serve(ctx)
 
-	s.WaitUntilReady()
+	s.waitUntilReady()
 
 	// And a request to the status API is sent
 	req, err := http.NewRequest(http.MethodGet, fmt.Sprintf("http://localhost:%d%s", port, statusOnlyErrorsAPIPath), nil)
@@ -173,7 +176,7 @@ func TestServe_Entity(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			// Given a running HTTP endpoint and an errored one (which times out)
-			port, err := network_helpers.TCPPort()
+			port, err := networkHelpers.TCPPort()
 			require.NoError(t, err)
 
 			r := status.NewReporter(ctx, l, []string{}, timeout, transport, tt.idProvide, "user-agent", "agent-key")
@@ -186,7 +189,7 @@ func TestServe_Entity(t *testing.T) {
 
 			go s.Serve(ctx)
 
-			s.WaitUntilReady()
+			s.waitUntilReady()
 
 			// And a request to the status API is sent
 			req, err := http.NewRequest(http.MethodGet, fmt.Sprintf("http://localhost:%d%s", port, statusEntityAPIPath), nil)
@@ -212,7 +215,7 @@ func TestServe_Entity(t *testing.T) {
 func TestServe_IngestData(t *testing.T) {
 	t.Parallel()
 
-	port, err := network_helpers.TCPPort()
+	port, err := networkHelpers.TCPPort()
 	require.NoError(t, err)
 
 	em := &testemit.RecordEmitter{}
@@ -227,7 +230,7 @@ func TestServe_IngestData(t *testing.T) {
 
 	payloadWritten := make(chan struct{})
 	go func() {
-		s.WaitUntilReady()
+		s.waitUntilReady()
 		client := http.Client{}
 		postReq, err := http.NewRequest("POST", fmt.Sprintf("http://localhost:%d%s", port, ingestAPIPath), bytes.NewReader(fixtures.FooBytes))
 		resp, err := client.Do(postReq)
@@ -286,7 +289,7 @@ func TestServe_IngestData_mTLS(t *testing.T) {
 		t.Run(testCase.name, func(t *testing.T) {
 			t.Parallel()
 
-			port, err := network_helpers.TCPPort()
+			port, err := networkHelpers.TCPPort()
 			require.NoError(t, err)
 
 			em := &testemit.RecordEmitter{}
@@ -304,7 +307,7 @@ func TestServe_IngestData_mTLS(t *testing.T) {
 
 			payloadWritten := make(chan struct{})
 			go func() {
-				s.WaitUntilReady()
+				s.waitUntilReady()
 				if testCase.sendCert {
 					// WaitUntilReady() is a no-op when mTLS is enabled, which causes the test to race sometimes.
 					// Sleeping one second is a dirty workaround to wait for the server to be ready.
@@ -359,6 +362,92 @@ func TestServe_IngestData_mTLS(t *testing.T) {
 			require.NoError(t, err)
 			assert.Equal(t, "unique foo", d.DataSet.PluginDataSet.Entity.Name)
 		})
+	}
+}
+
+func TestServer_ServeShouldEndSyncrhonouslyIfDisabled(t *testing.T) {
+	t.Parallel()
+
+	em := &testemit.RecordEmitter{}
+	srv, err := NewServer(&noopReporter{}, em)
+	require.NoError(t, err)
+	srv.Ingest.enabled = false
+	srv.Status.enabled = false
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	timer := time.NewTimer(500 * time.Millisecond)
+
+	go func() {
+		<-timer.C
+		t.Fatal("there should not be time for this to be executed")
+	}()
+
+	srv.Serve(ctx)
+	timer.Stop()
+}
+
+func Test_waitUntilReadyOrError_ShouldEndInCaseOfNoErrorButNotSuccess(t *testing.T) {
+	t.Parallel()
+
+	em := &testemit.RecordEmitter{}
+	srv, err := NewServer(&noopReporter{}, em)
+	require.NoError(t, err)
+
+	readinessProbeTimeout = time.Millisecond * 200
+
+	errCh := make(chan error, 1)
+	errCh <- nil
+	close(errCh)
+	err = srv.waitUntilReadyOrError("localhost", "/path", false, false, errCh)
+	assert.ErrorIs(t, err, ErrUrlUnreachable)
+}
+
+func TestServer_ServerErrorsShouldBeLogged(t *testing.T) {
+	t.Parallel()
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
+
+	port, err := networkHelpers.TCPPort()
+	require.NoError(t, err)
+
+	server := &http.Server{Addr: fmt.Sprintf("localhost:%d", port), Handler: nil}
+	defer server.Shutdown(ctx) // nolint:errcheck
+
+	go server.ListenAndServe() // nolint:errcheck
+	time.Sleep(time.Second * 2)
+
+	em := &testemit.RecordEmitter{}
+	srv, err := NewServer(&noopReporter{}, em)
+	require.NoError(t, err)
+	srv.Ingest.Enable("localhost", port)
+	srv.Status.Enable("localhost", port)
+
+	hook := logHelper.NewInMemoryEntriesHook([]logrus.Level{logrus.FatalLevel, logrus.ErrorLevel})
+	log.AddHook(hook)
+
+	srv.Serve(ctx)
+
+	var opError *net.OpError
+	expectedEntries := []logrus.Fields{
+		{"error": &opError, "component": "api", "message": "Ingest server error"},
+		{"error": &opError, "component": "api", "message": "error serving agent ingest"},
+		{"error": &opError, "component": "api", "message": "error serving agent status"},
+		{"error": &opError, "component": "api", "message": "unable to start Status-API"},
+	}
+	entries := hook.GetEntries()
+	assert.Len(t, entries, len(expectedEntries))
+
+	sort.SliceStable(entries, func(i, j int) bool {
+		return entries[i].Message < entries[j].Message
+	})
+
+	for i, entry := range entries {
+		assert.Equal(t, expectedEntries[i]["message"], entry.Message)
+		assert.Equal(t, expectedEntries[i]["component"], entry.Data["component"])
+		assert.ErrorAs(t, entry.Data["error"].(error), expectedEntries[i]["error"]) // nolint:forcetypeassert
 	}
 }
 
