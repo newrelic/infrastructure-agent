5a0f287aa0d338f145b2c0fe26b3f8f816f304fd
Roger Coll <rogercoll@protonmail.com>
2022-10-07 12:12:37 +0200

    feat: add kernel free memory

--- pkg/metrics/memory.go
+++ pkg/metrics/memory.go
@@ -19,14 +19,15 @@ type SwapSample struct {
 }
 
 type MemorySample struct {
-	MemoryTotal       float64 `json:"memoryTotalBytes"`
-	MemoryFree        float64 `json:"memoryFreeBytes"`
-	MemoryUsed        float64 `json:"memoryUsedBytes"`
-	MemoryFreePercent float64 `json:"memoryFreePercent"`
-	MemoryUsedPercent float64 `json:"memoryUsedPercent"`
-	MemoryCachedBytes float64 `json:"memoryCachedBytes"`
-	MemorySlabBytes   float64 `json:"memorySlabBytes"`
-	MemorySharedBytes float64 `json:"memorySharedBytes"`
+	MemoryTotal       float64  `json:"memoryTotalBytes"`
+	MemoryFree        float64  `json:"memoryFreeBytes"`
+	MemoryKernelFree  *float64 `json:"memoryKernelFreeBytes,omitempty"`
+	MemoryUsed        float64  `json:"memoryUsedBytes"`
+	MemoryFreePercent float64  `json:"memoryFreePercent"`
+	MemoryUsedPercent float64  `json:"memoryUsedPercent"`
+	MemoryCachedBytes float64  `json:"memoryCachedBytes"`
+	MemorySlabBytes   float64  `json:"memorySlabBytes"`
+	MemorySharedBytes float64  `json:"memorySharedBytes"`
 	SwapSample
 }
 
@@ -58,9 +59,14 @@ func (mm *MemoryMonitor) Sample() (result *MemorySample, err error) {
 		memoryUsedPercent = 100.0 - memoryFreePercent
 	}
 
+	toPtr := func(f float64) *float64 {
+		return &f
+	}
+
 	return &MemorySample{
 		MemoryTotal:       float64(memory.Total),
 		MemoryFree:        float64(memory.Available),
+		MemoryKernelFree:  toPtr(float64(memory.Free)),
 		MemoryUsed:        float64(memory.Used),
 		MemoryCachedBytes: float64(memory.Cached),
 		MemorySlabBytes:   float64(memory.Slab),
