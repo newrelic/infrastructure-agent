9820abcc39038ecf6f4ea7a96c9542a37a6f4f69
Cristian Ciutea <cristi.ciutea@gmail.com>
2022-10-24 18:35:37 +0200

    test

--- test/test.mk
+++ test/test.mk
@@ -47,7 +47,7 @@ ifndef NEW_RELIC_ACCOUNT_ID
 	@echo "NEW_RELIC_ACCOUNT_ID variable must be provided for test/automated/packaging"
 	exit 1
 endif
-	@ANSIBLE_DISPLAY_SKIPPED_HOSTS=NO ANSIBLE_DISPLAY_OK_HOSTS=NO ansible-playbook -f $(ANSIBLE_FORKS)  -i $(ANSIBLE_INVENTORY) --limit=$(LIMIT) -e nr_license_key=$(NR_LICENSE_KEY) -e nr_api_key=$(NEW_RELIC_API_KEY) -e nr_account_id=$(NEW_RELIC_ACCOUNT_ID) $(CURDIR)/test/packaging/ansible/test.yml
+	@ansible-playbook -f $(ANSIBLE_FORKS)  -i $(ANSIBLE_INVENTORY) --limit=$(LIMIT) -e nr_license_key=$(NR_LICENSE_KEY) -e nr_api_key=$(NEW_RELIC_API_KEY) -e nr_account_id=$(NEW_RELIC_ACCOUNT_ID) $(CURDIR)/test/packaging/ansible/test.yml
 
 .PHONY: test/automated/packaging-docker
 test/automated/packaging-docker:
