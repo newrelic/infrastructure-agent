9a1d1096c5185575cd25e15a481dd66cb6819f05
Cristian Ciutea <cristi.ciutea@gmail.com>
2022-10-28 10:33:55 +0200

    added aws logs script (#1480)
    * added aws logs script
    
    * cleanup
    
    * cleahup

--- .github/actions/external_runner/main.go
+++ .github/actions/external_runner/main.go
@@ -222,8 +222,11 @@ func printFargateTaskLogs(ctx context.Context, params Config, cfg aws.Config, ta
 func printLogsInfo(params Config, taskID string) {
 	source := fmt.Sprintf("%s/%s", params.CloudWatchLogsStreamName, taskID)
 	fmt.Fprintf(log.Writer(), "Fetching logs from: %s\n", source)
+	fmt.Fprintf(log.Writer(), "Tail logs:\n")
+	fmt.Fprintf(log.Writer(), "    tools/aws_logs.sh --group-name=/%s --stream-name=%s --tail\n",
+		params.CloudWatchLogsStreamName, source)
 	fmt.Fprintf(log.Writer(), "Download full logs:\n")
-	fmt.Fprintf(log.Writer(), "aws logs get-log-events --log-group-name /%s --log-stream-name %s --output text > %s.output.txt\n",
+	fmt.Fprintf(log.Writer(), "    tools/aws_logs.sh --group-name=/%s --stream-name=%s --output-file=%s.txt\n",
 		params.CloudWatchLogsStreamName, source, params.ActionID)
 }
 
