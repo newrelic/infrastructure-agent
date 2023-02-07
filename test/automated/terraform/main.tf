17d8691364d897b106ecd33032804c1f0ad5bb05
Roger Coll <rogercoll@protonmail.com>
2023-02-07 14:55:56 +0100

    feat: fix min tls version

--- test/automated/terraform/main.tf
+++ test/automated/terraform/main.tf
@@ -13,20 +13,9 @@ terraform {
 # ECS Cluster
 #########################################
 module "ecs" {
-  source = "registry.terraform.io/terraform-aws-modules/ecs/aws"
-  version = "3.5.0"
+  source = "./modules/"
 
-  name                               = var.cluster_name
-  capacity_providers                 = ["FARGATE"]
-  default_capacity_provider_strategy = [
-    {
-      capacity_provider = "FARGATE"
-    }
-  ]
-
-  tags = {
-    owning_team = "CAOS"
-  }
+  cluster_name                               = var.cluster_name
 }
 
 #########################################
