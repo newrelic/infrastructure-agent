191c1aef4e7b5258e258f0e2c53ce2aa78ff54be
Ruben Ruiz de Gauna <rubenruizdegauna@gmail.com>
2023-03-20 11:13:25 +0100

    NR-98861 test: get latest release for alerts

--- tools/spin-ec2/main_test.go
+++ tools/spin-ec2/main_test.go
@@ -27,6 +27,16 @@ func Test_previousCanaryVersion(t *testing.T) {
 			referenceVersion: "1.34.0",
 			expectedVersion:  "1.33.2",
 		},
+		{
+			name:             "second version does have previous",
+			referenceVersion: "1.12.1",
+			expectedVersion:  "1.12.0",
+		},
+		{
+			name:             "first version does not have previous",
+			referenceVersion: "1.12.0",
+			expectedVersion:  "previous version not found :(",
+		},
 	}
 
 	for i := range testCases {
