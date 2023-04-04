ecd939622edc4c3f62d824c3db5a6dfa8f48e16f
Cristian Ciutea <cristi.ciutea@gmail.com>
2023-04-04 16:56:50 +0200

    fixed test

--- internal/agent/inventory/patcher_test.go
+++ internal/agent/inventory/patcher_test.go
@@ -23,7 +23,7 @@ func TestPatcher_NeedsCleanup_NeverCleaned(t *testing.T) {
 
 func TestPatcher_NeedsCleanup_DefaultRemoveEntitiesPeriodExceeded(t *testing.T) {
 	b := BasePatcher{
-		lastClean: time.Now().Add(-defaultRemoveEntitiesPeriod + 1),
+		lastClean: time.Now().Add(-(defaultRemoveEntitiesPeriod + 1)),
 	}
 	assert.True(t, b.needsCleanup())
 	assert.False(t, b.needsCleanup())
@@ -38,7 +38,7 @@ func TestPatcher_NeedsCleanup_ConfigRemoveEntitiesPeriodExceeded(t *testing.T) {
 	}
 	assert.False(t, b.needsCleanup())
 
-	b.lastClean = b.lastClean.Add(-31 * time.Minute)
+	b.lastClean = b.lastClean.Add(-(30*time.Minute + 1))
 	assert.True(t, b.needsCleanup())
 	assert.False(t, b.needsCleanup())
 }
