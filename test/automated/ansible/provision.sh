9d9410d6eadfc202f09f12f0330339679ff094a7
Ruben Ruiz de Gauna <rubenruizdegauna@gmail.com>
2023-05-16 11:33:17 +0200

    force exit on provision

--- test/automated/ansible/provision.sh
+++ test/automated/ansible/provision.sh
@@ -19,25 +19,31 @@ function retry () {
   return "$status"
 }
 
-if [[ "$PLATFORM" != "macos" ]];then
-ANSIBLE_STDOUT_CALLBACK=selective \
-  ansible-playbook \
-  -i test/automated/ansible/inventory.local \
-  -e provision_host_prefix=$PROVISION_HOST_PREFIX \
-  -e output_inventory_ext=$ANSIBLE_INVENTORY \
-  -e ansible_password_windows=$ANSIBLE_PASSWORD_WINDOWS \
-  -e platform=$PLATFORM \
-  -f $ANSIBLE_FORKS \
-  test/automated/ansible/provision.yml
-fi
+exit 1
 
-ANSIBLE_DISPLAY_SKIPPED_HOSTS=NO retry ansible-playbook -f $ANSIBLE_FORKS -i $ANSIBLE_INVENTORY test/automated/ansible/install-requirements.yml
 
-if [[ "$PLATFORM" == "macos" || "$PLATFORM" == "all" ]];then
-  retry ansible-playbook \
-  -e macstadium_user=$MACSTADIUM_USER \
-  -e macstadium_sudo_pass=$MACSTADIUM_SUDO_PASS \
-  -e macstadium_pass=$MACSTADIUM_PASS \
-   -e output_inventory_macos=$ANSIBLE_INVENTORY \
-  test/automated/ansible/macos-canaries.yml
-fi
+#retry ansible-playbook -i test/automated/ansible/inventory.local test/automated/ansible/install-requirements.yml
+
+
+#if [[ "$PLATFORM" != "macos" ]];then
+#ANSIBLE_STDOUT_CALLBACK=selective \
+#  ansible-playbook \
+#  -i test/automated/ansible/inventory.local \
+#  -e provision_host_prefix=$PROVISION_HOST_PREFIX \
+#  -e output_inventory_ext=$ANSIBLE_INVENTORY \
+#  -e ansible_password_windows=$ANSIBLE_PASSWORD_WINDOWS \
+#  -e platform=$PLATFORM \
+#  -f $ANSIBLE_FORKS \
+#  test/automated/ansible/provision.yml
+#fi
+#
+#ANSIBLE_DISPLAY_SKIPPED_HOSTS=NO retry ansible-playbook -f $ANSIBLE_FORKS -i $ANSIBLE_INVENTORY test/automated/ansible/install-requirements.yml
+#
+#if [[ "$PLATFORM" == "macos" || "$PLATFORM" == "all" ]];then
+#  retry ansible-playbook \
+#  -e macstadium_user=$MACSTADIUM_USER \
+#  -e macstadium_sudo_pass=$MACSTADIUM_SUDO_PASS \
+#  -e macstadium_pass=$MACSTADIUM_PASS \
+#   -e output_inventory_macos=$ANSIBLE_INVENTORY \
+#  test/automated/ansible/macos-canaries.yml
+#fi
