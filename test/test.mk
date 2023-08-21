c68a65da43e5faccc67ea987522f53e4bd14e149
Roger Coll <rogercoll@protonmail.com>
2023-08-21 12:28:09 +0200

    NR-144355: install gpg for debian instances

--- test/test.mk
+++ test/test.mk
@@ -52,7 +52,7 @@ ifndef NEW_RELIC_ACCOUNT_ID
 	@echo "NEW_RELIC_ACCOUNT_ID variable must be provided for test/automated/packaging"
 	exit 1
 endif
-	@ANSIBLE_DISPLAY_SKIPPED_HOSTS=NO ANSIBLE_DISPLAY_OK_HOSTS=NO ansible-playbook -f $(ANSIBLE_FORKS)  -i $(ANSIBLE_INVENTORY) --limit=$(LIMIT) -e nr_license_key=$(NR_LICENSE_KEY) -e nr_api_key=$(NEW_RELIC_API_KEY) -e nr_account_id=$(NEW_RELIC_ACCOUNT_ID) -e REPO_ENDPOINT="http://nr-downloads-ohai-staging.s3-website-us-east-1.amazonaws.com/debianbookworm" $(CURDIR)/test/packaging/ansible/test.yml
+	@ANSIBLE_DISPLAY_SKIPPED_HOSTS=NO ANSIBLE_DISPLAY_OK_HOSTS=NO ansible-playbook -f $(ANSIBLE_FORKS)  -i $(ANSIBLE_INVENTORY) --limit=$(LIMIT) -e nr_license_key=$(NR_LICENSE_KEY) -e nr_api_key=$(NEW_RELIC_API_KEY) -e nr_account_id=$(NEW_RELIC_ACCOUNT_ID) $(CURDIR)/test/packaging/ansible/test.yml
 
 .PHONY: test/automated/packaging-docker
 test/automated/packaging-docker:
