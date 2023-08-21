fd1c4d60bd477bd1e648718f6e5fcc0c28fc0547
alvarocabanas <acabanas@newrelic.com>
2023-08-21 17:09:29 +0200

    Add windows build tack

--- pkg/ctl/sender/client_container_windows.go
+++ pkg/ctl/sender/client_container_windows.go
@@ -1,5 +1,8 @@
 // Copyright 2020 New Relic Corporation. All rights reserved.
 // SPDX-License-Identifier: Apache-2.0
+//go:build windows
+// +build windows
+
 package sender
 
 func NewContainerClient(dockerAPIVersion, containerdNamespace, containerID, runtime string) (Client, error) {
