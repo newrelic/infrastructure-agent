eed383d747dfae37861ddb718beb2028ca5d1600
Roger Coll <rogercoll@protonmail.com>
2023-10-23 12:24:59 +0200

    chore: bump golangci version

--- internal/integrations/v4/fixtures/integration.sh
+++ internal/integrations/v4/fixtures/integration.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env sh
 
+# Avoid later deserialization problems when receiving a windows path
 Argument=$(echo "$1" | sed 's|\\|/|g')
 
 echo '{"name":"com.newrelic.test","protocol_version":"1","integration_version":"1.0.0","metrics":[{"event_type":"TestSample","value":"'$Argument'"}]}'
