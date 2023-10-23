6717a7be7e39c58518632873c7a939d103a466cc
Roger Coll <rogercoll@protonmail.com>
2023-10-23 12:08:47 +0200

    feat: replace backslashes

--- internal/integrations/v4/fixtures/integration.sh
+++ internal/integrations/v4/fixtures/integration.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env sh
 
-echo '{"name":"com.newrelic.test","protocol_version":"1","integration_version":"1.0.0","metrics":[{"event_type":"TestSample","value":"'$1'"}]}'
+Argument=$(echo "$1" | sed 's|\\|/|g')
+
+echo '{"name":"com.newrelic.test","protocol_version":"1","integration_version":"1.0.0","metrics":[{"event_type":"TestSample","value":"'$Argument'"}]}'
