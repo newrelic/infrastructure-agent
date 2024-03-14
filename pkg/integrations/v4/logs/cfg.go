49e063ebad952fa6e66483638efeed575e08db6b
Roger Coll <rogercoll@protonmail.com>
2024-03-14 09:36:25 +0100

    use unexported field

--- pkg/integrations/v4/logs/cfg.go
+++ pkg/integrations/v4/logs/cfg.go
@@ -101,7 +101,7 @@ type LogCfg struct {
 	Fluentbit      *LogExternalFBCfg `yaml:"fluentbit"`
 	Winlog         *LogWinlogCfg     `yaml:"winlog"`
 	Winevtlog      *LogWinevtlogCfg  `yaml:"winevtlog"`
-	TargetFilesCnt int
+	targetFilesCnt int
 }
 
 // LogSyslogCfg logging integration config from customer defined YAML, specific for the Syslog input plugin
@@ -283,8 +283,8 @@ func NewFBConf(loggingCfgs LogsCfg, logFwdCfg *config.LogForward, entityGUID, ho
 
 	totalFiles := 0
 	for i, block := range loggingCfgs {
-		loggingCfgs[i].TargetFilesCnt = getTotalTargetFilesForPath(block)
-		totalFiles += loggingCfgs[i].TargetFilesCnt
+		loggingCfgs[i].targetFilesCnt = getTotalTargetFilesForPath(block)
+		totalFiles += loggingCfgs[i].targetFilesCnt
 		input, filters, external, err := parseConfigBlock(block, logFwdCfg.HomeDir, fbOSConfig)
 		if err != nil {
 			return
@@ -316,7 +316,7 @@ func NewFBConf(loggingCfgs LogsCfg, logFwdCfg *config.LogForward, entityGUID, ho
 		cfgLogger.Warn(warningMessage)
 
 		for _, logCfg := range loggingCfgs {
-			cfgLogger.Trace(fmt.Sprintf("FilePath: %s :::: TargetFilesCount: %d", logCfg.File, logCfg.TargetFilesCnt))
+			cfgLogger.Trace(fmt.Sprintf("FilePath: %s :::: TargetFilesCount: %d", logCfg.File, logCfg.targetFilesCnt))
 		}
 	}
 
