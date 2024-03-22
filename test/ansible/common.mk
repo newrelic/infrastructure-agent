9c28268c99103631ac4e1aa79f28353b392fc4ff
Ruben Ruiz de Gauna <rubenruizdegauna@proton.me>
2024-03-22 11:45:20 +0100

    reorder ansible dependency in makefiles

--- test/ansible/common.mk
+++ test/ansible/common.mk
@@ -4,11 +4,13 @@ REQUIREMENTS_FILE ?= $(ANSIBLE_FOLDER)/requirements.yml
 .PHONY: ansible/dependencies
 ansible/dependencies: $(ROLES_PATH) $(COLLECTIONS_PATH)
 ifdef CROWDSTRIKE_REPO_SSH_KEY
+	@echo "crowdstrike ssh-key present"
 	eval $$(ssh-agent -s) && \
 	ssh-add $(CROWDSTRIKE_REPO_SSH_KEY) && \
 	ansible-galaxy role install --force -r $(REQUIREMENTS_FILE) && \
 	ansible-galaxy collection install --force -r $(REQUIREMENTS_FILE)
 else
+	@echo "crowdstrike ssh-key not present"
 	ansible-galaxy role install --force -r $(REQUIREMENTS_FILE)
 	ansible-galaxy collection install --force -r $(REQUIREMENTS_FILE)
 endif
