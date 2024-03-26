b20a3407dacc34f6a28106024a66a14468405afa
Ruben Ruiz de Gauna <rubenruizdegauna@proton.me>
2024-03-26 09:22:47 +0100

    remove unnecessary nolint directive

--- pkg/backend/identityapi/metadata_test.go
+++ pkg/backend/identityapi/metadata_test.go
@@ -38,7 +38,7 @@ func TestMetadataHarvesterDefault(t *testing.T) {
 
 	for i := range testCases {
 		testCase := testCases[i]
-		t.Run(testCase.name, func(t *testing.T) { //nolint:paralleltest
+		t.Run(testCase.name, func(t *testing.T) {
 			// Set environment variables
 			for envVarKey, envVar := range testCase.envVars {
 				err := os.Setenv(envVarKey, envVar)
