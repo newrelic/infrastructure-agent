ffcdce040582ddd0b1729fc0e080841e3fadb161
alvarocabanas <acabanas@newrelic.com>
2024-08-05 09:30:53 +0200

    test

--- build/sign.sh
+++ build/sign.sh
@@ -8,15 +8,11 @@ set -e
 #
 #
 
-# Set gpg-agent defaults
+# Allow loopback pinentry for GPG
 GNUPGHOME="/root/.gnupg"
-
-echo "${GPG_PASSPHRASE}" > "${GNUPGHOME}/gpg-passphrase"
-echo "passphrase-file ${GNUPGHOME}/gpg-passphrase" >> "$GNUPGHOME/gpg.conf"
 echo 'allow-loopback-pinentry' >> "${GNUPGHOME}/gpg-agent.conf"
-echo 'pinentry-mode loopback' >> "${GNUPGHOME}/gpg.conf"
-echo 'use-agent' >> "${GNUPGHOME}/gpg.conf"
 echo RELOADAGENT | gpg-connect-agent
+
 
 # Sign RPM's
 echo "===> Create .rpmmacros to sign rpm's from Goreleaser"
@@ -44,13 +40,22 @@ for rpm_file in $(find -regex ".*\.\(rpm\)");do
   if [[ $rpm_file =~ $sles_regex ]]; then
     rpmsign -v --addsign --rpmv3 $rpm_file
   else
-    rpmsign -v --addsign $rpm_file
+    mkfifo in out
+    rpmsign -v --addsign $rpm_file <in >out &
+    echo ${GPG_PASSPHRASE} >in
+    read -r reply <out
   fi
 
   echo "===> Sign verification $rpm_file"
   rpm -v --checksig $rpm_file
 done
 
+# Sign DEB's
+echo "${GPG_PASSPHRASE}" > "${GNUPGHOME}/gpg-passphrase"
+echo "passphrase-file ${GNUPGHOME}/gpg-passphrase" >> "$GNUPGHOME/gpg.conf"
+echo 'pinentry-mode loopback' >> "${GNUPGHOME}/gpg.conf"
+echo 'use-agent' >> "${GNUPGHOME}/gpg.conf"
+echo RELOADAGENT | gpg-connect-agent
 
 for deb_file in $(find -regex ".*\.\(deb\)");do
   echo "===> Signing $deb_file"
