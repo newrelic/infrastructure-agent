6641502aed38ffd07714b4b78f9d19f2e967a3e0
David Sánchez <davidsanchez@newrelic.com>
2024-09-04 14:17:51 +0100

    ci: support SLES 15.6 (#1916)
    * ci: support SLES 15.6
    
    * ci: add molecule tests for missing sles targets
    
    * fix: makefile recipe spacing
    
    * ci: add new SLES to multiarch make target
    
    * fix: remove upload of arm pkg (not existing)
    
    * ci: target file for arm
    
    * ci: pass sles-15.6 to machines list
    
    * chore: clean up commented content

--- build/release.mk
+++ build/release.mk
@@ -186,6 +186,7 @@ generate-goreleaser-amd64:
   		$(CURDIR)/build/goreleaser/linux/sles_153_amd64.yml\
   		$(CURDIR)/build/goreleaser/linux/sles_154_amd64.yml\
   		$(CURDIR)/build/goreleaser/linux/sles_155_amd64.yml\
+  		$(CURDIR)/build/goreleaser/linux/sles_156_amd64.yml\
   		 > $(GORELEASER_CONFIG_LINUX)
 
 .PHONY : generate-goreleaser-arm
@@ -210,6 +211,7 @@ generate-goreleaser-amd64:
   		$(CURDIR)/build/goreleaser/linux/sles_153_arm.yml\
   		$(CURDIR)/build/goreleaser/linux/sles_154_arm.yml\
   		$(CURDIR)/build/goreleaser/linux/sles_155_arm.yml\
+  		$(CURDIR)/build/goreleaser/linux/sles_156_arm.yml\
   		 > $(GORELEASER_CONFIG_LINUX)
 
 .PHONY : generate-goreleaser-arm64
@@ -234,6 +236,7 @@ generate-goreleaser-arm64:
   		$(CURDIR)/build/goreleaser/linux/sles_153_arm64.yml\
   		$(CURDIR)/build/goreleaser/linux/sles_154_arm64.yml\
   		$(CURDIR)/build/goreleaser/linux/sles_155_arm64.yml\
+  		$(CURDIR)/build/goreleaser/linux/sles_156_arm64.yml\
   		 > $(GORELEASER_CONFIG_LINUX)
 
 .PHONY : generate-goreleaser-legacy
@@ -306,6 +309,9 @@ generate-goreleaser-multiarch:
   		$(CURDIR)/build/goreleaser/linux/sles_155_amd64.yml\
   		$(CURDIR)/build/goreleaser/linux/sles_155_arm.yml\
   		$(CURDIR)/build/goreleaser/linux/sles_155_arm64.yml\
+  		$(CURDIR)/build/goreleaser/linux/sles_156_amd64.yml\
+  		$(CURDIR)/build/goreleaser/linux/sles_156_arm.yml\
+  		$(CURDIR)/build/goreleaser/linux/sles_156_arm64.yml\
   		 > $(GORELEASER_CONFIG_LINUX)
 
 .PHONY : generate-goreleaser-for-docker
