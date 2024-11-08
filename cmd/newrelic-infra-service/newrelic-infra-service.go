5368be6fc8b3055c1dad322f1609608b80aaff70
alvarocabanas <acabanas@newrelic.com>
2024-11-08 16:10:59 +0100

    Simplify the fips cmd binaries

--- cmd/newrelic-infra-service/newrelic-infra-service.go
+++ cmd/newrelic-infra-service/newrelic-infra-service.go
@@ -2,8 +2,6 @@
 // SPDX-License-Identifier: Apache-2.0
 //go:generate goversioninfo
 
-//go:build !fips
-
 package main
 
 import (
---
+++ cmd/newrelic-infra/fips.go
@@ -0,0 +1,10 @@
+// Copyright 2020 New Relic Corporation. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+//go:generate goversioninfo
+
+//go:build fips
+// +build fips
+
+package main
+
+import _ "crypto/tls/fipsonly"
