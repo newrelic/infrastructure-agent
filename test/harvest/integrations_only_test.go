800bcd2f995a8c11d00955bcd9c8222f711bb98c
Abhishu Raina <araina@newrelic.com>
2025-07-23 12:21:19 +0530

    increase timeout for function harvest test

--- test/harvest/integrations_only_test.go
+++ test/harvest/integrations_only_test.go
@@ -5,44 +5,59 @@
 
 package harvest
 
-// func TestIntegrationsOnlyMode(t *testing.T) {
-// 	t.Parallel()
+import (
+	"net/http"
+	"testing"
+	"time"
 
-// 	const timeout = 5 * time.Second
+	"github.com/newrelic/infrastructure-agent/pkg/backend/inventoryapi"
+	"github.com/newrelic/infrastructure-agent/pkg/config"
+	"github.com/newrelic/infrastructure-agent/pkg/entity"
+	"github.com/newrelic/infrastructure-agent/pkg/plugins"
+	fixture "github.com/newrelic/infrastructure-agent/test/fixture/inventory"
+	"github.com/newrelic/infrastructure-agent/test/infra"
+	ihttp "github.com/newrelic/infrastructure-agent/test/infra/http"
+	"github.com/stretchr/testify/assert"
+)
 
-// 	testClient := ihttp.NewRequestRecorderClient()
-// 	agt := infra.NewAgent(testClient.Client, func(config *config.Config) {
-// 		config.DisplayName = "my_display_name"
-// 		config.IsIntegrationsOnly = true
-// 		config.HeartBeatSampleRate = 1
-// 	})
-// 	agt.Context.SetAgentIdentity(entity.Identity{ID: 10, GUID: "abcdef"})
+func TestIntegrationsOnlyMode(t *testing.T) {
+	t.Parallel()
 
-// 	if err := plugins.RegisterPlugins(agt); err != nil {
-// 		assert.FailNow(t, "fatal error while registering plugins")
-// 	}
-// 	go func() {
-// 		_ = agt.Run()
-// 	}()
+	const timeout = 15 * time.Second
 
-// 	var req http.Request
-// 	select {
-// 	case req = <-testClient.RequestCh:
-// 		agt.Terminate()
-// 	case <-time.After(timeout):
-// 		agt.Terminate()
-// 		assert.FailNow(t, "timeout while waiting for a response")
-// 	}
+	testClient := ihttp.NewRequestRecorderClient()
+	agt := infra.NewAgent(testClient.Client, func(config *config.Config) {
+		config.DisplayName = "my_display_name"
+		config.IsIntegrationsOnly = true
+		config.HeartBeatSampleRate = 1
+	})
+	agt.Context.SetAgentIdentity(entity.Identity{ID: 10, GUID: "abcdef"})
 
-// 	fixture.AssertRequestContainsInventoryDeltas(t, req, []*inventoryapi.RawDelta{
-// 		{
-// 			Source:   "metadata/infra_agent",
-// 			ID:       1,
-// 			FullDiff: true,
-// 			// Checking some common services that should exist in any linux host
-// 			Diff: map[string]interface{}{
-// 				"integrations_only": bool(true),
-// 			},
-// 		},
-// 	})
-// }
+	if err := plugins.RegisterPlugins(agt); err != nil {
+		assert.FailNow(t, "fatal error while registering plugins")
+	}
+	go func() {
+		_ = agt.Run()
+	}()
+
+	var req http.Request
+	select {
+	case req = <-testClient.RequestCh:
+		agt.Terminate()
+	case <-time.After(timeout):
+		agt.Terminate()
+		assert.FailNow(t, "timeout while waiting for a response")
+	}
+
+	fixture.AssertRequestContainsInventoryDeltas(t, req, []*inventoryapi.RawDelta{
+		{
+			Source:   "metadata/infra_agent",
+			ID:       1,
+			FullDiff: true,
+			// Checking some common services that should exist in any linux host
+			Diff: map[string]interface{}{
+				"integrations_only": bool(true),
+			},
+		},
+	})
+}
