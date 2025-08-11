d1caf4aa1cea8c748bcd2240ddc3086ebe725f9f
Ujwal Sah <usah@newrelic.com>
2025-08-11 14:18:57 +0530

    feat: bug fix with cpu darwin

---
+++ pkg/metrics/cpu_darwin.go
@@ -0,0 +1,20 @@
+//go:build !windows
+// +build !windows
+
+// Copyright 2020 New Relic Corporation. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+package metrics
+
+import (
+	"github.com/newrelic/infrastructure-agent/internal/agent"
+	"github.com/shirou/gopsutil/v3/cpu"
+)
+
+// NewCPUMonitor creates a new CPU monitor using gopsutil for non-Windows platforms
+func NewCPUMonitor(context agent.AgentContext) *CPUMonitor {
+	return &CPUMonitor{
+		context:        context,
+		cpuTimes:       cpu.Times,
+		windowsMonitor: nil,
+	}
+}
