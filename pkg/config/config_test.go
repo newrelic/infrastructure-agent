c3dcee921e88b345f18fccd6727e31b97f382cd7
nravada <nravada@newrelic.com>
2025-09-29 08:25:42 +0530

    fix: Address linter errors (#2136)

--- pkg/config/config_test.go
+++ pkg/config/config_test.go
@@ -686,6 +686,7 @@ license_key: "xxx"
 
 	tmp, err := createTestFile(yamlData)
 	require.NoError(t, err)
+
 	defer os.Remove(tmp.Name())
 
 	cfg, err := LoadConfig(tmp.Name())
@@ -695,13 +696,15 @@ license_key: "xxx"
 	assert.Equal(t, "xxx", cfg.License)
 	assert.Equal(t, "agent.log", cfg.LogFile)
 	assert.Equal(t, "json", cfg.LogFormat)
-	assert.Equal(t, true, cfg.LogToStdout)
+	assert.True(t, cfg.LogToStdout)
 	assert.Equal(t, 5, cfg.SmartVerboseModeEntryLimit)
 	assert.Equal(t, SmartVerboseLogging, cfg.Verbose)
 	assert.Equal(t, LogLevelSmart, cfg.Log.Level)
 }
 
 func TestLoadYamlConfig_withLogFormat(t *testing.T) {
+	t.Parallel()
+
 	yamlData := []byte(`
 log:
   file: agent.log
@@ -713,6 +716,7 @@ license_key: "xxx"
 
 	tmp, err := createTestFile(yamlData)
 	require.NoError(t, err)
+
 	defer os.Remove(tmp.Name())
 
 	cfg, err := LoadConfig(tmp.Name())
@@ -722,7 +726,7 @@ license_key: "xxx"
 	assert.Equal(t, "xxx", cfg.License)
 	assert.Equal(t, "agent.log", cfg.LogFile)
 	assert.Equal(t, "json", cfg.LogFormat)
-	assert.Equal(t, true, cfg.LogToStdout)
+	assert.True(t, cfg.LogToStdout)
 	assert.Equal(t, 5, cfg.SmartVerboseModeEntryLimit)
 	assert.Equal(t, LogLevelWarn, cfg.Log.Level)
 }
@@ -800,10 +804,10 @@ func TestLoadLogConfig_BackwardsCompatability(t *testing.T) {
 		{"Trace and forward enabled", Config{Log: LogConfig{Level: "trace", Forward: toPtr(true)}}, TraceTroubleshootLogging},
 	}
 
-	//nolint:rangevarcopies
 	for i := range logConfigs {
 		tt := &logConfigs[i]
 		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
 			assert.Equal(t, NonVerboseLogging, tt.c.Verbose)
 			tt.c.loadLogConfig()
 			assert.Equal(t, tt.verbose, tt.c.Verbose)
@@ -830,10 +834,10 @@ func TestLoadLogConfig_Populate(t *testing.T) {
 		{"Trace Verbose enabled and file", Config{Verbose: 4, LogFile: "agent.log"}, LogConfig{Level: LogLevelTrace, File: "agent.log", ToStdout: boolPtr(false), Forward: boolPtr(false), ExcludeFilters: LogFilters{"traces": []interface{}{"supervisor", "feature", "process"}, "component": []interface{}{"integration-errors"}}, SmartLevelEntryLimit: intPtr(0)}},
 	}
 
-	//nolint:rangevarcopies
 	for i := range configs {
 		tt := &configs[i]
 		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
 			tt.c.loadLogConfig()
 			assert.Equal(t, tt.expectedLogConfig, tt.c.Log)
 		})
