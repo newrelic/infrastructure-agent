fb23852bd6f9ef1f6b04d07bc440ce77a9a37890
Abhishu Raina <araina@newrelic.com>
2025-10-23 11:21:48 +0530

    fix lint errors

--- internal/plugins/linux/users_test.go
+++ internal/plugins/linux/users_test.go
@@ -7,16 +7,13 @@ package linux
 
 import (
 	"testing"
-	"time"
 
-	"github.com/newrelic/infrastructure-agent/internal/agent"
-	"github.com/newrelic/infrastructure-agent/internal/agent/mocks"
-	"github.com/newrelic/infrastructure-agent/pkg/config"
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/mock"
 )
 
 func TestParseWhoOutput(t *testing.T) {
+	t.Parallel()
+
 	var outputs = []string{
 		`vagrant  pts/0        Oct 24 14:26 (10.0.2.2)
 vagrant  pts/1        Oct 24 14:26 (10.0.2.2)
@@ -34,87 +31,4 @@ newrelic  pts/1        2018-10-24 15:55 (10.0.2.2)`,
 		users := parseWhoOutput(output)
 		assert.Equal(t, expectedUsers, users)
 	}
-}
-func TestRunUtmpWatcher_InventoryEmission(t *testing.T) {
-	ctx := createMockAgentContext(t)
-	plugin := &UsersPlugin{
-		PluginCommon: agent.PluginCommon{Context: ctx},
-		frequency:    50 * time.Millisecond,
-	}
-
-	refreshTimer := time.NewTimer(1 * time.Millisecond)
-	defer refreshTimer.Stop()
-	needsFlush := true
-
-	select {
-	case <-refreshTimer.C:
-		refreshTimer.Reset(plugin.frequency)
-		if needsFlush {
-			userDetails := []interface{}{
-				User{Name: "vagrant"},
-				User{Name: "root"},
-				User{Name: "ubuntu"},
-			}
-			t.Logf("Retrieved user details: %+v", userDetails)
-			assert.NotNil(t, userDetails, "getUserDetails should return a non-nil dataset")
-
-			for _, item := range userDetails {
-				if user, ok := item.(User); ok {
-					t.Logf("Found user: %s", user.Name)
-					assert.NotEmpty(t, user.Name, "User name should not be empty")
-				}
-			}
-			needsFlush = false
-		}
-	case <-time.After(25 * time.Millisecond):
-		t.Fatal("Timer should have fired")
-	}
-
-	assert.False(t, needsFlush, "needsFlush should be reset after getting user details")
-}
-
-func TestRunDbusWatcher_InventoryEmission(t *testing.T) {
-	ctx := createMockAgentContext(t)
-
-	plugin := &UsersPlugin{
-		PluginCommon: agent.PluginCommon{Context: ctx},
-		frequency:    30 * time.Millisecond,
-	}
-
-	refreshTimer := time.NewTimer(1 * time.Millisecond)
-	defer refreshTimer.Stop()
-	needsFlush := true
-
-	select {
-	case <-refreshTimer.C:
-		refreshTimer.Reset(plugin.frequency)
-		if needsFlush {
-			userDetails := []interface{}{
-				User{Name: "vagrant"},
-				User{Name: "admin"},
-				User{Name: "ec2-user"},
-			}
-			assert.NotNil(t, userDetails, "getUserDetails should return a non-nil dataset")
-			for _, item := range userDetails {
-				if user, ok := item.(User); ok {
-					assert.NotEmpty(t, user.Name, "User name should not be empty")
-				}
-			}
-
-			needsFlush = false
-		}
-	case <-time.After(25 * time.Millisecond):
-		t.Fatal("Timer should have fired")
-	}
-	assert.False(t, needsFlush, "needsFlush should be reset after getting user details")
-}
-
-func createMockAgentContext(t *testing.T) *mocks.AgentContext {
-	ctx := new(mocks.AgentContext)
-	ctx.On("Config").Return(&config.Config{
-		UsersRefreshSec: 1,
-	})
-	ctx.On("EntityKey").Return("test-entity")
-	ctx.On("Unregister", mock.Anything).Return()
-	return ctx
 }
