ad547053595b236cb539ac1db902744a481e4bbb
mbolla22 <mbolla@newrelic.com>
2025-11-11 18:24:27 +0530

    Added RHEL10 support

--- build/sign.sh
+++ build/sign.sh
@@ -28,13 +28,16 @@ echo "%_gpg_name ${GPG_MAIL}" >> ~/.rpmmacros
 echo "%_signature gpg" >> ~/.rpmmacros
 echo "%_gpg_path /root/.gnupg" >> ~/.rpmmacros
 echo "%_gpgbin /usr/bin/gpg" >> ~/.rpmmacros
-echo "%__gpg_sign_cmd   %{__gpg} gpg --no-verbose --no-armor --passphrase ${GPG_PASSPHRASE} --no-secmem-warning --digest-algo sha256 -u "%{_gpg_name}" -sbo %{__signature_filename} %{__plaintext_filename}" >> ~/.rpmmacros
+echo "%__gpg_sign_cmd   %{__gpg} gpg --no-verbose --no-armor --passphrase ${GPG_PASSPHRASE} --no-secmem-warning --digest-algo sha256 --cert-digest-algo sha256 -u "%{_gpg_name}" -sbo %{__signature_filename} %{__plaintext_filename}" >> ~/.rpmmacros
 
 echo "===> Importing GPG private key from GHA secrets..."
 printf %s ${GPG_PRIVATE_KEY_BASE64} | base64 -d | gpg --batch --import -
 
+echo "===> Adding binding signature for RHEL 10 compatibility..."
+gpg --batch --yes --passphrase "${GPG_PASSPHRASE}" --quick-sign-key ${GPG_MAIL}
+
 echo "===> Importing GPG signature, needed from Goreleaser to verify signature"
-gpg --export -a ${GPG_MAIL} > /tmp/RPM-GPG-KEY-${GPG_MAIL}
+gpg --export -a --cert-digest-algo sha256 ${GPG_MAIL} > /tmp/RPM-GPG-KEY-${GPG_MAIL}
 rpm --import /tmp/RPM-GPG-KEY-${GPG_MAIL}
 
 cd dist
